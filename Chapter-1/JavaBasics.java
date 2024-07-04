e08b5a499cabc5b53b13175a224b7ab8516a7337
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-07-04 22:03:10 +0530

    Outputs in Java

--- Chapter-1/JavaBasics.java
+++ Chapter-1/JavaBasics.java
@@ -1,5 +1,10 @@
 public class JavaBasics{
     public static void main(String args[]){
-        
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.print("Hello World\n");
+        System.out.print("Hello World\n");
+        System.out.print("Hello World\n");
     }
 }
