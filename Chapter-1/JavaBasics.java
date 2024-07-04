57ae7c3987fe6aa09a8dfac925a4d3b7502abeb0
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-07-04 22:17:36 +0530

    Printing Patterns

--- Chapter-1/JavaBasics.java
+++ Chapter-1/JavaBasics.java
@@ -1,10 +1,16 @@
 public class JavaBasics{
     public static void main(String args[]){
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.print("Hello World\n");
-        System.out.print("Hello World\n");
-        System.out.print("Hello World\n");
+        // System.out.println('Hello World');
+        // System.out.println("Hello World");
+        // System.out.println("Hello World");
+        // System.out.print("Hello World\n");
+        // System.out.print("Hello World\n");
+        // System.out.print("Hello World\n");
+
+        // System.out.print("****\n***\n**\n*");
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
     }
 }
