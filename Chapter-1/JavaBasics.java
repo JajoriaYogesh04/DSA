d8179b3a740a11185161903205656bd624b73e2c
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-07-05 06:23:26 +0530

    Variables in Java

--- Chapter-1/JavaBasics.java
+++ Chapter-1/JavaBasics.java
@@ -8,9 +8,17 @@ public class JavaBasics{
         // System.out.print("Hello World\n");
 
         // System.out.print("****\n***\n**\n*");
-        System.out.println("****");
-        System.out.println("***");
-        System.out.println("**");
-        System.out.println("*");
+        // System.out.println("****");
+        // System.out.println("***");
+        // System.out.println("**");
+        // System.out.println("*");
+        
+        int a = 10;
+        int b = 20;
+        String name= "Tony Stark";
+        a = 50;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(name);
     }
 }
