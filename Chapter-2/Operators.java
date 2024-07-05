c0890438d6bdaa955adeac7cebbfcf51a8077841
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-07-05 21:54:06 +0530

    Binary Operators

---
+++ Chapter-2/Operators.java
@@ -0,0 +1,13 @@
+
+
+public class Operators {
+    public static void main(String args[]) {
+        int A=10;       //Binary Operators: Two operands
+        int B=5;
+        System.out.println("A+B = "+(A+B));
+        System.out.println("A-B = "+(A-B));
+        System.out.println("A*B = "+(A*B));
+        System.out.println("A/B = "+(A/B));
+        System.out.println("A%B = "+(A%B));
+    }
+}
