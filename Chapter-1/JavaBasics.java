6eac4ae1771523ff610b7899cb73f16366655154
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-07-05 07:07:32 +0530

    Data types in Java

--- Chapter-1/JavaBasics.java
+++ Chapter-1/JavaBasics.java
@@ -1,3 +1,5 @@
+import javax.swing.plaf.synth.SynthStyle;
+
 public class JavaBasics{
     public static void main(String args[]){
         // System.out.println('Hello World');
@@ -13,12 +15,23 @@ public class JavaBasics{
         // System.out.println("**");
         // System.out.println("*");
         
-        int a = 10;
-        int b = 20;
-        String name= "Tony Stark";
-        a = 50;
+        // int a = 10;
+        // int b = 20;
+        // String name= "Tony Stark";
+        // a = 50;
+        // System.out.println(a);
+        // System.out.println(b);
+        // System.out.println(name);
+
+        byte a= 10;
+        boolean choice= true;
+        char ch = 'a';
+        int num= 4546;
+        double num2= 3.145576798;
         System.out.println(a);
-        System.out.println(b);
-        System.out.println(name);
+        System.out.println(choice);
+        System.out.println(ch);
+        System.out.println(num);
+        System.out.println(num2);
     }
 }
