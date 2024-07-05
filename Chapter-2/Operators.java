122e6ad5ac3f9546d3c542925dfa5fed1b678f6b
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-07-05 22:15:39 +0530

    Relational Operators

--- Chapter-2/Operators.java
+++ Chapter-2/Operators.java
@@ -10,12 +10,21 @@ public class Operators {
         // System.out.println("A/B = "+(A/B));
         // System.out.println("A%B = "+(A%B));
 
-        int a= 10;      //Unary Operators
-        // int b= ++a;
-        // int b= a++;
-        // int b= a--;
-        int b= --a;
-        System.out.println(a);
-        System.out.println(b);
+        // int a= 10;      //Unary Operators
+        // // int b= ++a;
+        // // int b= a++;
+        // // int b= a--;
+        // int b= --a;
+        // System.out.println(a);
+        // System.out.println(b);
+
+        int a=10;       //Relational Operators
+        int b=5;
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a>b);
+        System.out.println(a>=b);
+        System.out.println(a<b);
+        System.out.println(a<=b);
     }
 }
