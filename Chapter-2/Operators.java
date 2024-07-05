bc0fab92ae326ceff3356e36854daee7e53957b7
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-07-05 22:04:27 +0530

    Unary Operators

--- Chapter-2/Operators.java
+++ Chapter-2/Operators.java
@@ -2,12 +2,20 @@
 
 public class Operators {
     public static void main(String args[]) {
-        int A=10;       //Binary Operators: Two operands
-        int B=5;
-        System.out.println("A+B = "+(A+B));
-        System.out.println("A-B = "+(A-B));
-        System.out.println("A*B = "+(A*B));
-        System.out.println("A/B = "+(A/B));
-        System.out.println("A%B = "+(A%B));
+        // int A=10;       //Binary Operators: Two operands
+        // int B=5;
+        // System.out.println("A+B = "+(A+B));
+        // System.out.println("A-B = "+(A-B));
+        // System.out.println("A*B = "+(A*B));
+        // System.out.println("A/B = "+(A/B));
+        // System.out.println("A%B = "+(A%B));
+
+        int a= 10;      //Unary Operators
+        // int b= ++a;
+        // int b= a++;
+        // int b= a--;
+        int b= --a;
+        System.out.println(a);
+        System.out.println(b);
     }
 }
