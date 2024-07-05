9301e742a5591974fbecea2e1280fb00781c64c3
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-07-05 11:50:28 +0530

    Inputs in Java

--- Chapter-1/JavaBasics.java
+++ Chapter-1/JavaBasics.java
@@ -1,5 +1,8 @@
+import java.util.*;
+
 public class JavaBasics{
     public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
         // System.out.println('Hello World');
         // System.out.println("Hello World");
         // System.out.println("Hello World");
@@ -32,9 +35,24 @@ public class JavaBasics{
         // System.out.println(num);
         // System.out.println(num2);
 
-        int a= 30;
-        int b= 20;
-        int sum= a+b;
-        System.out.println(sum);
+        // int a= 30;  //Calculate sum of two numbers
+        // int b= 40;
+        // int sum= a+b;
+        // System.out.println(sum);
+
+        String input= sc.next();
+        System.out.println(input);
+
+        String input2= sc.nextLine();
+        System.out.println(input2);
+
+        int input3= sc.nextInt();
+        System.out.println(input3);
+
+        float input4= sc.nextFloat();
+        System.out.println(input4);
+
+        boolean input5= sc.nextBoolean();
+        System.out.println(input5);
     }
 }
