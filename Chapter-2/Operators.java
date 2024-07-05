e85126bf636c441fc5ffec5fdeccdf16b186d9c0
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-07-05 22:33:59 +0530

    assignment Operators

--- Chapter-2/Operators.java
+++ Chapter-2/Operators.java
@@ -27,15 +27,22 @@ public class Operators {
         // System.out.println(a<b);
         // System.out.println(a<=b);
 
-        System.out.println(true && true);       //Logical Operators
-        System.out.println(true && false);
-        System.out.println(false && true);
-        System.out.println(false && false);
-        System.out.println(true || true);
-        System.out.println(true || false);
-        System.out.println(false || true);
-        System.out.println(false || false);
-        System.out.println(! true);
-        System.out.println(! false);
+        // System.out.println(true && true);       //Logical Operators
+        // System.out.println(true && false);
+        // System.out.println(false && true);
+        // System.out.println(false && false);
+        // System.out.println(true || true);
+        // System.out.println(true || false);
+        // System.out.println(false || true);
+        // System.out.println(false || false);
+        // System.out.println(! true);
+        // System.out.println(! false);
+
+        int a=10;       //Assignment operators
+        System.out.println(a+=5);
+        System.out.println(a-=10);
+        System.out.println(a*=5);
+        System.out.println(a/=5);
+        System.out.println(a%=5);
     }
 }
