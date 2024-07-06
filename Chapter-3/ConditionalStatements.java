ab9d2686f60558d987e9118993a598a6d926da9d
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-07-06 22:37:30 +0530

    Largest of two numbers

--- Chapter-3/ConditionalStatements.java
+++ Chapter-3/ConditionalStatements.java
@@ -1,15 +1,24 @@
 
 public class ConditionalStatements {
     public static void main(String args[]) {
-        int age= 15;
-        if (age>=18) {
-            System.out.print("ADULT");
-        }
-        if (age>=13 && age<18) {
-            System.out.print("TEENAGER");
+        // int age= 15;
+        // if (age>=18) {
+        //     System.out.print("ADULT");
+        // }
+        // if (age>=13 && age<18) {
+        //     System.out.print("TEENAGER");
+        // }
+        // else{
+        //     System.out.print("CHILD");
+        // }
+
+        int a= 35;
+        int b= 20;
+        if (a>b) {
+            System.out.println("LARGEST: A");
         }
         else{
-            System.out.print("CHILD");
+            System.out.println("LARGEST: B");
         }
     }
 }
