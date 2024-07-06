53c6d6df925acb97f8d1c6df42caa829ac4f25f6
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-07-06 22:47:19 +0530

    Even and Odd numbers

--- Chapter-3/ConditionalStatements.java
+++ Chapter-3/ConditionalStatements.java
@@ -1,7 +1,9 @@
+import java.util.*;
 
 public class ConditionalStatements {
     public static void main(String args[]) {
-        // int age= 15;
+        Scanner sc = new Scanner(System.in);
+        // int age= 15;     IF-else
         // if (age>=18) {
         //     System.out.print("ADULT");
         // }
@@ -12,13 +14,21 @@ public class ConditionalStatements {
         //     System.out.print("CHILD");
         // }
 
-        int a= 35;
-        int b= 20;
-        if (a>b) {
-            System.out.println("LARGEST: A");
+        // int a= 35;       //Largest of two numbers
+        // int b= 20;
+        // if (a>b) {
+        //     System.out.println("LARGEST: A");
+        // }
+        // else{
+        //     System.out.println("LARGEST: B");
+        // }
+
+        int num= sc.nextInt();     //Even and odd numbers
+        if(num%2==0){
+            System.out.println("EVEN");
         }
         else{
-            System.out.println("LARGEST: B");
+            System.out.println("ODD");
         }
     }
 }
