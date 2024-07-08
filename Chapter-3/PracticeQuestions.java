a75089ccb64468036cfdb6a2688d735f8c40e60e
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-07-08 21:56:55 +0530

    Practice question 2

--- Chapter-3/PracticeQuestions.java
+++ Chapter-3/PracticeQuestions.java
@@ -4,16 +4,25 @@ public class PracticeQuestions {
     public static void main(String args[]) {
         Scanner sc= new Scanner(System.in);
 
-        System.out.print("Enter Number: ");     //Practice Question 1
-        int num= sc.nextInt();
-        if(num>0){
-            System.out.println("POSITIVE INTERGER");
-        }
-        else if(num<0){
-            System.out.println("NEGATIVE INTEGER");
+        // System.out.print("Enter Number: ");     //Practice Question 1
+        // int num= sc.nextInt();
+        // if(num>0){
+        //     System.out.println("POSITIVE INTERGER");
+        // }
+        // else if(num<0){
+        //     System.out.println("NEGATIVE INTEGER");
+        // }
+        // else{
+        //     System.out.println("ZERO");
+        // }
+
+        System.out.print("Enter Temperature: ");        //Practice Question 2
+        int temp= sc.nextInt();
+        if(temp>100){
+            System.out.println("You have fever");
         }
         else{
-            System.out.println("ZERO");
+            System.out.println("You don't have fever");
         }
     }
 }
