6ff313fce3b1388714d1edc5b32c6c9009e8d5c4
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-07-08 13:43:00 +0530

    Largest of three numbers

--- Chapter-3/ConditionalStatements.java
+++ Chapter-3/ConditionalStatements.java
@@ -1,60 +1,95 @@
-import java.util.*;
+// import java.util.*;
 
+// public class ConditionalStatements {
+//     public static void main(String args[]) {
+//         Scanner sc = new Scanner(System.in);
+//         // int age= 15;     IF-else
+//         // if (age>=18) {
+//         //     System.out.print("ADULT");
+//         // }
+//         // if (age>=13 && age<18) {
+//         //     System.out.print("TEENAGER");
+//         // }
+//         // else{
+//         //     System.out.print("CHILD");
+//         // }
+
+//         // int a= 35;       //Largest of two numbers
+//         // int b= 20;
+//         // if (a>b) {
+//         //     System.out.println("LARGEST: A");
+//         // }
+//         // else{
+//         //     System.out.println("LARGEST: B");
+//         // }
+
+//         // int num= sc.nextInt();     //Even and odd numbers
+//         // if(num%2==0){
+//         //     System.out.println("EVEN");
+//         // }
+//         // else{
+//         //     System.out.println("ODD");
+//         // }
+
+//         // int age= sc.nextInt();      //Else-if
+//         // if(age>=18){
+//         //     System.out.println("ADULT");
+//         // }
+//         // else if(age>=13 && age<18){
+//         //     System.out.println("TEENAGER");
+//         // }
+//         // else{
+//         //     System.out.println("CHILD");
+//         // }
+
+//         // int income= sc.nextInt();       //Income tax calculator
+//         // float tax=0;
+//         // if(income<500000){
+//         //     tax=0;
+//         //     System.out.println("Tax: "+ tax);
+//         // }
+//         // else if(income>=500000 && income<1000000){
+//         //     tax=(0.20f*income);
+//         //     System.out.println("Tax: "+ tax);
+//         // }
+//         // else{
+//         //     tax=(0.30f*income);
+//         //     System.out.println("Tax: "+ tax);
+//         // }
+//     }
+// }
+
+import java.util.Scanner;       
 public class ConditionalStatements {
     public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        // int age= 15;     IF-else
-        // if (age>=18) {
-        //     System.out.print("ADULT");
+        Scanner sc= new Scanner(System.in);
+        int a= sc.nextInt();
+        int b= sc.nextInt();
+        int c= sc.nextInt();
+        // if(a<b){
+        //     if(b>c){
+        //         System.out.println("Greatest: B");
+        //     }
+        //     else if(c>b){
+        //         System.out.println("Greatest: C");
+        //     }
         // }
-        // if (age>=13 && age<18) {
-        //     System.out.print("TEENAGER");
+        // else if(a>b){
+        //     if(a>c){
+        //         System.out.println("Greatest: A");
+        //     }
+        //     else if(c>a){
+        //         System.out.println("Greatest: C");
+        //     }
         // }
-        // else{
-        //     System.out.print("CHILD");
-        // }
-
-        // int a= 35;       //Largest of two numbers
-        // int b= 20;
-        // if (a>b) {
-        //     System.out.println("LARGEST: A");
-        // }
-        // else{
-        //     System.out.println("LARGEST: B");
-        // }
-
-        // int num= sc.nextInt();     //Even and odd numbers
-        // if(num%2==0){
-        //     System.out.println("EVEN");
-        // }
-        // else{
-        //     System.out.println("ODD");
-        // }
-
-        // int age= sc.nextInt();      //Else-if
-        // if(age>=18){
-        //     System.out.println("ADULT");
-        // }
-        // else if(age>=13 && age<18){
-        //     System.out.println("TEENAGER");
-        // }
-        // else{
-        //     System.out.println("CHILD");
-        // }
-
-        int income= sc.nextInt();       //Income tax calculator
-        float tax=0;
-        if(income<500000){
-            tax=0;
-            System.out.println("Tax: "+ tax);
+        if((a>=b) && (a>=c)){
+            System.out.println("Greatest: A");
         }
-        else if(income>=500000 && income<1000000){
-            tax=(0.20f*income);
-            System.out.println("Tax: "+ tax);
+        else if(b>=c){
+            System.out.println("Greatest: B");
         }
         else{
-            tax=(0.30f*income);
-            System.out.println("Tax: "+ tax);
+            System.out.println("Greatest: C");
         }
     }
 }
