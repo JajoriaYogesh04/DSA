3451fcc58b4c927c334964228aa3cf67e2b76fac
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-07-08 22:04:17 +0530

    Practice Question 3

--- Chapter-3/PracticeQuestions.java
+++ Chapter-3/PracticeQuestions.java
@@ -16,13 +16,33 @@ public class PracticeQuestions {
         //     System.out.println("ZERO");
         // }
 
-        System.out.print("Enter Temperature: ");        //Practice Question 2
-        int temp= sc.nextInt();
-        if(temp>100){
-            System.out.println("You have fever");
-        }
-        else{
-            System.out.println("You don't have fever");
+        // System.out.print("Enter Temperature: ");        //Practice Question 2
+        // int temp= sc.nextInt();
+        // if(temp>100){
+        //     System.out.println("You have fever");
+        // }
+        // else{
+        //     System.out.println("You don't have fever");
+        // }
+
+        System.out.print("Enter week day number: ");        //Practice Question 3
+        int day= sc.nextInt();
+        switch(day){
+            case 1: System.out.println("Sunday");
+            break;
+            case 2: System.out.println("Monday");
+            break;
+            case 3: System.out.println("Tuesday");
+            break;
+            case 4: System.out.println("Wednesday");
+            break;
+            case 5: System.out.println("Thursday");
+            break;
+            case 6: System.out.println("Friday");
+            break;
+            case 7: System.out.println("Saturday");
+            break;
+            default: System.out.println("ERROR: Check input");
         }
     }
 }
