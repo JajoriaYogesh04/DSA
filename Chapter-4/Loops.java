3a359e0f922db3ab6ff84f6d0206e89d085e4a97
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-07-10 15:24:17 +0530

    While lopp

---
+++ Chapter-4/Loops.java
@@ -0,0 +1,11 @@
+public class Loops {
+    public static void main(String args[]){
+
+        int counter= 0;     //While lopp
+        while(counter < 100){
+            System.out.println("Hello World");
+            counter++;
+        }
+        System.out.println("HW 100 times");
+    }
+}
