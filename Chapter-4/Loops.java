61dd98dcda87757901e575f6260cd4dd35343e3c
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-07-10 15:34:50 +0530

    Printing numbers 1 to n

--- Chapter-4/Loops.java
+++ Chapter-4/Loops.java
@@ -1,5 +1,8 @@
+import java.util.*;
+
 public class Loops {
     public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
 
         // int counter= 0;     //While lopp
         // while(counter < 100){
@@ -8,8 +11,16 @@ public class Loops {
         // }
         // System.out.println("HW 100 times");
 
-        int counter=1;      //Numbers 1 to 10
-        while(counter<=10){
+        // int counter=1;      //Numbers 1 to 10
+        // while(counter<=10){
+        //     System.out.print(counter + " ");
+        //     counter++;
+        // }
+
+        System.out.print("Enter N: ");
+        int n= sc.nextInt();
+        int counter= 1;
+        while (counter<=n) {
             System.out.print(counter + " ");
             counter++;
         }
