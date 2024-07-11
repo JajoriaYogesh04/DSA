42a6c83a1bb088c19667388935da34d6cfa0f602
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-07-11 22:38:59 +0530

    Inverted star pattern

--- Chapter-5/Patterns.java
+++ Chapter-5/Patterns.java
@@ -4,10 +4,19 @@ public class Patterns {
     public static void main(String args[]) {
         Scanner sc= new Scanner(System.in);
         
-        System.out.print("Enter number: ");         //Star pattern
+        // System.out.print("Enter number: ");         //Star pattern
+        // int n= sc.nextInt();
+        // for(int i=1; i<=n; i++){
+        //     for(int j=1; j<=i; j++){
+        //         System.out.print(" * ");
+        //     }
+        //     System.out.println();
+        // }
+
+        System.out.print("Enter number: ");         //Inverted star pattern
         int n= sc.nextInt();
         for(int i=1; i<=n; i++){
-            for(int j=1; j<=i; j++){
+            for(int j=1; j<=n-i+1; j++){
                 System.out.print(" * ");
             }
             System.out.println();
