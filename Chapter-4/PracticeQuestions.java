1a4e17b1f673d41bf0a7bf211bc7910cd3714a60
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-07-11 16:24:07 +0530

    Practice Question 2

--- Chapter-4/PracticeQuestions.java
+++ Chapter-4/PracticeQuestions.java
@@ -4,24 +4,32 @@ public class PracticeQuestions {
     public static void main(String args[]){
         Scanner sc= new Scanner(System.in);
 
-        int num;            //Practice Question 2
-        int evenSum=0;
-        int oddSum=0;
-        int choice=1;
-        while(choice!=0){
-            System.out.print("Enter Number: ");
-            num= sc.nextInt();
-            // System.out.println(num);
-            if(num%2==0){
-                evenSum+=num;
-            }
-            else{
-                oddSum+=num;s
-            }
-            System.out.print("Enter 0 to exit & 1 to continue: ");
-            choice= sc.nextInt();
+        // int num;            //Practice Question 2
+        // int evenSum=0;
+        // int oddSum=0;
+        // int choice=1;
+        // while(choice!=0){
+        //     System.out.print("Enter Number: ");
+        //     num= sc.nextInt();
+        //     // System.out.println(num);
+        //     if(num%2==0){
+        //         evenSum+=num;
+        //     }
+        //     else{
+        //         oddSum+=num;s
+        //     }
+        //     System.out.print("Enter 0 to exit & 1 to continue: ");
+        //     choice= sc.nextInt();
+        // }
+        // System.out.println("Even Sum: "+evenSum);
+        // System.out.println("Odd Sum: "+oddSum);
+
+        System.out.print("Enter Number: ");         //Practice Question 2
+        int num= sc.nextInt();
+        int fact=1;
+        for(int i=1; i<=num; i++){
+            fact*=i;
         }
-        System.out.println("Even Sum: "+evenSum);
-        System.out.println("Odd Sum: "+oddSum);
+        System.out.println("Factorial: "+fact);
     }
 }
