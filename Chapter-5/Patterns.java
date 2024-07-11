c1933ed61b88a67b3227bfc66beffe5cd5256b64
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-07-11 22:44:16 +0530

    Half pyramid pattern

--- Chapter-5/Patterns.java
+++ Chapter-5/Patterns.java
@@ -13,11 +13,20 @@ public class Patterns {
         //     System.out.println();
         // }
 
-        System.out.print("Enter number: ");         //Inverted star pattern
+        // System.out.print("Enter number: ");         //Inverted star pattern
+        // int n= sc.nextInt();
+        // for(int i=1; i<=n; i++){
+        //     for(int j=1; j<=n-i+1; j++){
+        //         System.out.print(" * ");
+        //     }
+        //     System.out.println();
+        // }
+
+        System.out.print("Enter number: ");         //Half pyramid pattern
         int n= sc.nextInt();
         for(int i=1; i<=n; i++){
-            for(int j=1; j<=n-i+1; j++){
-                System.out.print(" * ");
+            for(int j=1; j<=i; j++){
+                System.out.print(" "+j+" ");
             }
             System.out.println();
         }
