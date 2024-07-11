e7234c9cfc239826e804dadff662b2813cce6d81
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-07-11 16:15:50 +0530

    Practice Question 2

---
+++ Chapter-4/PracticeQuestions.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class PracticeQuestions {
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+
+        int num;            //Practice Question 2
+        int evenSum=0;
+        int oddSum=0;
+        int choice=1;
+        while(choice!=0){
+            System.out.print("Enter Number: ");
+            num= sc.nextInt();
+            // System.out.println(num);
+            if(num%2==0){
+                evenSum+=num;
+            }
+            else{
+                oddSum+=num;s
+            }
+            System.out.print("Enter 0 to exit & 1 to continue: ");
+            choice= sc.nextInt();
+        }
+        System.out.println("Even Sum: "+evenSum);
+        System.out.println("Odd Sum: "+oddSum);
+    }
+}
