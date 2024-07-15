a63293ce01c59bfef795e198aaf6ce19c7d7e7ea
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-07-15 16:22:19 +0530

    Syntax of functions

--- Chapter-6/Functions.java
+++ Chapter-6/Functions.java
@@ -1,5 +1,13 @@
 public class Functions{
+
+    public static void printHelloWorld(){
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        return;
+    }
+
     public static void main(String args[]){
-        System.out.println("Chapter 6");
+        printHelloWorld();
     }
 }
