524361471492e329871e4b7de5e4a36ee9c4d00b
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-07-15 16:33:18 +0530

    Functions with parameters

--- Chapter-6/Functions.java
+++ Chapter-6/Functions.java
@@ -1,13 +1,29 @@
+import java.util.*;
+
 public class Functions{
 
-    public static void printHelloWorld(){
+    public static void printHelloWorld(){           //Syntax of functions
         System.out.println("Hello World");
         System.out.println("Hello World");
         System.out.println("Hello World");
         return;
     }
 
+    public static int getSum(int a, int b){         
+        int sum= a+b;
+        return sum;
+    }
+
     public static void main(String args[]){
-        printHelloWorld();
+        Scanner sc= new Scanner(System.in);
+
+        // printHelloWorld();
+
+        System.out.print("Enter first number: ");           //Functions with parameters        
+        int num1= sc.nextInt();
+        System.out.print("Enter second number: ");
+        int num2= sc.nextInt();
+        int sum= getSum(num1,num2);
+        System.out.println("Sum: "+ sum);
     }
 }
