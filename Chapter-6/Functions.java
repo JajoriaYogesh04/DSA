15ac40abb4b68b9c5e08c80374f0097850c1feca
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-07-16 07:12:32 +0530

    Swapping(Call by value)

--- Chapter-6/Functions.java
+++ Chapter-6/Functions.java
@@ -14,16 +14,33 @@ public class Functions{
         return sum;
     }
 
+    public static void swap(int a, int b){
+        int temp;
+        temp= a;
+        a= b;
+        b= temp;
+        System.out.println(a+" "+b);
+        return;
+    }
+
     public static void main(String args[]){
         Scanner sc= new Scanner(System.in);
 
         // printHelloWorld();
 
-        System.out.print("Enter first number: ");           //Functions with parameters        
-        int num1= sc.nextInt();
+        // System.out.print("Enter first number: ");           //Functions with parameters        
+        // int num1= sc.nextInt();
+        // System.out.print("Enter second number: ");
+        // int num2= sc.nextInt();
+        // int sum= getSum(num1,num2);
+        // System.out.println("Sum: "+ sum);
+
+        System.out.print("Enter first number: ");           //Swapping(Call by value)             
+        int a= sc.nextInt();
         System.out.print("Enter second number: ");
-        int num2= sc.nextInt();
-        int sum= getSum(num1,num2);
-        System.out.println("Sum: "+ sum);
+        int b= sc.nextInt();
+        System.out.println(a+" "+b);
+        swap(a, b);
+        System.out.println(a+" "+b);
     }
 }
