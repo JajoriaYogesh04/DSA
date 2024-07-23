d2eae98d5d620c85c9f6715654afdf48bb8306ba
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-07-23 23:13:42 +0530

    Inverted Rotated Half Pyramid

---
+++ Chapter-7/InvertedRotatedHalfPyramid.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class InvertedRotatedHalfPyramid {
+
+    public static void invertedRotatedHalfPyramid(int l){
+        for(int i= 1; i<= l; i++){
+            for(int j=1; j<= l; j++){
+                if(j<=l-i){
+                    System.out.print("   ");
+                }
+                else{
+                    System.out.print(" * ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+
+        System.out.print("Enter Length: ");
+        int length= sc.nextInt();
+        invertedRotatedHalfPyramid(length);
+    }
+}
