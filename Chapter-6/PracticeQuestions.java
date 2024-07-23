a4c8d0d27ad76b507c9e0cde5b73add2817701de
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-07-23 21:42:18 +0530

    Practice Question 2

--- Chapter-6/PracticeQuestions.java
+++ Chapter-6/PracticeQuestions.java
@@ -5,16 +5,35 @@ public class PracticeQuestions {
         return (a+b+c)/3;
     }
 
+    public static boolean isEven(int n){
+        if(n%2==0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
     public static void main(String args[]){
         Scanner sc= new Scanner(System.in);
 
-        System.out.print("Enter first number: ");                   //Practice Question 1
-        int a= sc.nextInt();
-        System.out.print("Enter second number: ");
-        int b= sc.nextInt();
-        System.out.print("Enter third number: ");
-        int c= sc.nextInt();
-        int avg= avg(a, b, c);
-        System.out.println("Average: "+avg);
+        // System.out.print("Enter first number: ");                   //Practice Question 1
+        // int a= sc.nextInt();
+        // System.out.print("Enter second number: ");
+        // int b= sc.nextInt();
+        // System.out.print("Enter third number: ");
+        // int c= sc.nextInt();
+        // int avg= avg(a, b, c);
+        // System.out.println("Average: "+avg);
+
+        System.out.print("Enter Number: ");                         //Practice Question 2
+        int num= sc.nextInt();
+        boolean isEven= isEven(num);
+        if(isEven){
+            System.out.println(num+" is EVEN");
+        }
+        else{
+            System.out.println(num+" is ODD");
+        }
     }
 }
