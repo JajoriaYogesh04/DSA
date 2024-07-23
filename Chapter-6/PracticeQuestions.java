5807bee0a7db3169c91d824fdc5e694d0857e281
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-07-23 22:04:54 +0530

    Practice Question 3

--- Chapter-6/PracticeQuestions.java
+++ Chapter-6/PracticeQuestions.java
@@ -14,6 +14,27 @@ public class PracticeQuestions {
         }
     }
 
+    public static int reverse(int num){
+        int rev= 0;
+        while(num>0){
+            int lastDigit= num%10;
+            rev= (rev*10)+lastDigit;
+            num/=10;
+        }
+        System.out.println(rev);
+        return rev;
+    }
+
+    public static boolean isPalindrome(int num){
+        int rev= reverse(num);
+        if(num==rev){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
     public static void main(String args[]){
         Scanner sc= new Scanner(System.in);
 
@@ -26,14 +47,23 @@ public class PracticeQuestions {
         // int avg= avg(a, b, c);
         // System.out.println("Average: "+avg);
 
-        System.out.print("Enter Number: ");                         //Practice Question 2
+        // System.out.print("Enter Number: ");                         //Practice Question 2
+        // int num= sc.nextInt();
+        // boolean isEven= isEven(num);
+        // if(isEven){
+        //     System.out.println(num+" is EVEN");
+        // }
+        // else{
+        //     System.out.println(num+" is ODD");
+        // }
+
+        System.out.print("Enter Number: ");                           //Practice Question 3
         int num= sc.nextInt();
-        boolean isEven= isEven(num);
-        if(isEven){
-            System.out.println(num+" is EVEN");
+        if(isPalindrome(num)){
+            System.out.println(num+" is a palindrome");
         }
         else{
-            System.out.println(num+" is ODD");
+            System.out.println(num+" is NOT a palindrome");
         }
     }
 }
