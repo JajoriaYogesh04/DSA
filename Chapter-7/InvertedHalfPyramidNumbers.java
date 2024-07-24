058c9acbc12e2a83a987b9ee33dc219f4c23ca55
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-07-24 09:40:49 +0530

    Inverted hald pyramid with numbers pattern

---
+++ Chapter-7/InvertedHalfPyramidNumbers.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class InvertedHalfPyramidNumbers {
+
+    public static void invertedHalfPyramidNumbers(int n){
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n-i+1; j++){
+                System.out.print(" "+j+" ");
+            }
+            for(int j=1; j<=i-1; j++){
+                System.out.print("   ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+
+        System.out.print("Enter Number: ");
+        int num= sc.nextInt();
+        invertedHalfPyramidNumbers(num);
+    }
+}
