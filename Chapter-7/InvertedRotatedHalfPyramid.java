7512aae37884b2c66799ce55b4d14883d58be001
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-07-24 09:21:23 +0530

    Modified Inverted rotated half pyramid pattern

--- Chapter-7/InvertedRotatedHalfPyramid.java
+++ Chapter-7/InvertedRotatedHalfPyramid.java
@@ -3,14 +3,23 @@ import java.util.*;
 public class InvertedRotatedHalfPyramid {
 
     public static void invertedRotatedHalfPyramid(int l){
-        for(int i= 1; i<= l; i++){
-            for(int j=1; j<= l; j++){
-                if(j<=l-i){
-                    System.out.print("   ");
-                }
-                else{
-                    System.out.print(" * ");
-                }
+        // for(int i= 1; i<= l; i++){
+        //     for(int j=1; j<= l; j++){
+        //         if(j<=l-i){
+        //             System.out.print("   ");
+        //         }
+        //         else{
+        //             System.out.print(" * ");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+        for(int i= 1; i<=l; i++){
+            for(int j=1; j<=l-i; j++){
+                System.out.print("   ");
+            }
+            for(int j=1; j<=i; j++){
+                System.out.print(" * ");
             }
             System.out.println();
         }
