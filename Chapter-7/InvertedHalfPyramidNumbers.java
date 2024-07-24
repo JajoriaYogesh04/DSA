d475f46ea41ebad4061e912c748b5443ec730796
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-07-24 09:44:56 +0530

    Modified inverted half pyramid with numbers

--- Chapter-7/InvertedHalfPyramidNumbers.java
+++ Chapter-7/InvertedHalfPyramidNumbers.java
@@ -7,9 +7,9 @@ public class InvertedHalfPyramidNumbers {
             for(int j=1; j<=n-i+1; j++){
                 System.out.print(" "+j+" ");
             }
-            for(int j=1; j<=i-1; j++){
-                System.out.print("   ");
-            }
+            // for(int j=1; j<=i-1; j++){
+            //     System.out.print("   ");
+            // }
             System.out.println();
         }
     }
