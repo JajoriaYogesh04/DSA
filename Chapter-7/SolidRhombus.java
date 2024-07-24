556521be616f007bef3bc3b6afd05e7f6bbd03e9
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-07-24 12:36:04 +0530

    Solid Rhombus Pattern

---
+++ Chapter-7/SolidRhombus.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class SolidRhombus {
+
+    public static void solidRhombus(int n){
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n-i; j++){
+                System.out.print("   ");
+            }
+            for(int j=1; j<=n; j++){
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+
+        System.out.print("Enter length: ");
+        int num= sc.nextInt();
+        solidRhombus(num);
+    }
+}
