1c31d3f95fce8196ddbcc2955f52f0c52f025a70
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-07-27 11:10:22 +0530

    Input Output and Update in array

--- Chapter-8/Array.java
+++ Chapter-8/Array.java
@@ -8,5 +8,20 @@ public class Array {
         int numbers[]= {21, 23, 49, 89, 78, 59};
         String fruits[]= {"Mango", "Apple", "Banana"};
 
+        System.out.print("Enter marks: ");
+        marks[0]= sc.nextInt();
+        System.out.print("Enter marks: ");
+        marks[1]= sc.nextInt();
+        System.out.print("Enter marks: ");
+        marks[2]= sc.nextInt();
+        System.out.println("Subject 1: "+marks[0]);
+        System.out.println("Subject 2: "+marks[1]);
+        System.out.println("Subject 3: "+marks[2]);
+        // marks[2]= 70;
+        // System.out.println("Subject 3(updated): "+marks[2]);
+        System.out.println("Percentage: "+ (marks[0]+marks[1]+marks[2])/3+"%");
+
+        System.out.println(marks.length);
+        System.out.println(numbers.length);
     }
 }
