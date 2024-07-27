90586ab28a9f7850fdc8b819c3769cadeed1dc84
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-07-27 12:41:37 +0530

    Linear search on menu

--- Chapter-8/LinearSearch.java
+++ Chapter-8/LinearSearch.java
@@ -9,18 +9,40 @@ public class LinearSearch {
         }
         return -1;
     }
+
+    public static int linearSearchMenu(String menu[], String item){
+        for(int i=0; i<menu.length; i++){
+            if(menu[i].equals(item)){
+                return i;
+            }
+        }
+        return -1;
+    }
+
     public static void main(String args[]){
         Scanner sc= new Scanner(System.in);
         
-        int arr[]= {2,4,6,8,10,12,14,16,18,20};
-        System.out.print("Enter element you want to search: ");
-        int n= sc.nextInt();
-        int index= linearSearch(arr, n);
+        // int arr[]= {2,4,6,8,10,12,14,16,18,20};
+        // System.out.print("Enter element you want to search: ");
+        // // int n= sc.nextInt();
+        // int index= linearSearch(arr, n);
+        // if(index==-1){
+        //     System.out.println("NOT FOUND");
+        // }
+        // else{
+        //     System.out.println("Index: "+index);
+        // }
+
+        String menu[]= {"Samosa", "Kachori", "Pizza", "Burger", "Namkeen"};
+        System.out.print("You want: ");
+        String item= sc.nextLine();
+        int index= linearSearchMenu(menu, item);
         if(index==-1){
             System.out.println("NOT FOUND");
         }
         else{
             System.out.println("Index: "+index);
         }
+
     }
 }
