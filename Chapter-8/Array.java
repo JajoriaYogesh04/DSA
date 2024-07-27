df4abbae847e22d5725dc51f4e1f7294e536b9de
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-07-27 10:04:09 +0530

    Array Creation

--- Chapter-8/Array.java
+++ Chapter-8/Array.java
@@ -4,6 +4,9 @@ public class Array {
     public static void main(String args[]){
         Scanner sc= new Scanner(System.in);
 
-        
+        int marks[]= new int[50];               //Empty array           
+        int numbers[]= {21, 23, 49, 89, 78, 59};
+        String fruits[]= {"Mango", "Apple", "Banana"};
+
     }
 }
