34eb98ce3b50e6425919c8e6b167a6474ea0a2aa
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-07-29 10:23:34 +0530

    Minimum and Maximum Sum of Subarrays

--- Chapter-8/SubArrays.java
+++ Chapter-8/SubArrays.java
@@ -3,6 +3,8 @@ import java.util.*;
 public class SubArrays {
     public static void subArrays(int arr[]){
         int ts= 0;
+        int minSum= Integer.MAX_VALUE;
+        int maxSum= Integer.MIN_VALUE;
         for(int i=0; i<arr.length; i++){
             for(int j=i; j<arr.length; j++){
                 int sum=0;
@@ -10,12 +12,20 @@ public class SubArrays {
                     System.out.print("["+arr[k]+"]");
                     sum+=arr[k];
                 }
+                if(sum<minSum){
+                    minSum=sum;
+                }
+                if(sum>maxSum){
+                    maxSum=sum;
+                }
                 System.out.print("="+sum+"    ");
                 ts++;
             }
             System.out.println();
         }
         System.out.println("Total Subarrays: "+ts);
+        System.out.println("Minimum Sum Subarrays: "+minSum);
+        System.out.println("Maximum Sum Subarrays: "+maxSum);
     }
     public static void inputArray(int arr[]){
         Scanner sc= new Scanner(System.in);
