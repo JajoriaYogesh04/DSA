2c19e830e2176c4d033ae2614b97e0afc04d5ec2
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-08-03 17:30:53 +0530

    Bubble Sort

---
+++ Chapter-10/BubbleSort.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class BubbleSort {
+    public static void bubbleSort(int arr[]){
+        int swap= 0;
+        for(int i=0; i<=arr.length-2; i++){
+            for(int j=0; j<=arr.length-2-i; j++){
+                if(arr[j]>arr[j+1]){
+                    int temp= arr[j+1];
+                    arr[j+1]= arr[j];
+                    arr[j]= temp;
+                    swap++;
+                }
+            }
+        }
+        System.out.print("Number of Swaps: "+swap+"\n");
+    }
+    public static void printArray(int arr[]){
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void inputArray(int arr[]){
+        Scanner sc= new Scanner(System.in);
+        for(int i=0; i<arr.length; i++){
+            System.out.print("Enter: ");
+            arr[i]= sc.nextInt();
+        }
+    }
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter Array Length: ");
+        int length= sc.nextInt();
+        int arr[]= new int [length];
+        inputArray(arr);
+        bubbleSort(arr);
+        printArray(arr);
+    }
+}
