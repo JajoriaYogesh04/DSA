fce4c28664c8746e7fb6a8b76019408ab577bae8
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-08-07 07:08:05 +0530

    Binary Search in Searching along row

--- Chapter-11/Search.java
+++ Chapter-11/Search.java
@@ -8,6 +8,32 @@ public class Search{
             }
         }
     }
+    public static int BinarySearchRow(int arr[], int key){
+        int start= 0;
+        int end= arr.length-1;
+        int mid=0;
+        while(start<=end){
+            mid= (start+end)/2;
+            if(key==arr[mid]){
+                return mid;
+            }
+            if(key>=arr[mid]){
+                start= mid+1;
+            }
+            else{
+                end= mid-1;
+            }
+        }
+        return -1;
+    }
+    public static void BinarySearch(int arr[][], int key){
+        for(int i=0; i<arr.length; i++){
+            int colIdx= BinarySearchRow(arr[i], key);
+            if(colIdx!=-1){
+                System.out.print("("+i+","+colIdx+")");
+            }
+        }
+    }
     public static void main(String args[]){
         int arr[][]= {
             {10,20,30,40},
@@ -15,7 +41,10 @@ public class Search{
             {27,29,37,48},
             {32,33,39,50}
         };
-        int key= 37;
-        brutForce(arr, key);
+        int brr[]= {1,2,3,4};
+        int key= 39;
+        // brutForce(arr, key);
+        // BinarySearchRow(brr, 4);
+        BinarySearch(arr, key);
     }
 }
