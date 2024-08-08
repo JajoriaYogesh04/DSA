0006f9c351d30815d22261cdf2fd26fb9ea14740
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-08-08 23:03:03 +0530

    Strings in Java

---
+++ Chapter-12/Strings.java
@@ -0,0 +1,9 @@
+public class Strings{
+    public static void main(String ars[]){
+        char arr[]= {'a', 'b', 'c', 'd'};
+        String str= "abcd";
+        System.out.println(str);
+        String str2= new String("abcd");
+        System.out.println(str2);
+    }
+}
