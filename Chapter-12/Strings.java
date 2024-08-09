5ab1ba4e75a8be2b8bbda3c21fea83f1773a1fc0
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-08-09 16:30:19 +0530

    String Length

--- Chapter-12/Strings.java
+++ Chapter-12/Strings.java
@@ -1,9 +1,21 @@
+import java.util.*;
 public class Strings{
     public static void main(String ars[]){
-        char arr[]= {'a', 'b', 'c', 'd'};
-        String str= "abcd";
-        System.out.println(str);
-        String str2= new String("abcd");
-        System.out.println(str2);
+        Scanner sc= new Scanner(System.in);
+
+        // char arr[]= {'a', 'b', 'c', 'd'};
+        // String str= "abcd";
+        // System.out.println(str);
+        // String str2= new String("abcd");
+        // System.out.println(str2);
+
+        // System.out.print("Enter Word: ");
+        // String word= sc.next();
+        // System.out.println(word);
+        System.out.print("Enter Sentence: ");
+        String sentence= sc.nextLine();
+        System.out.println(sentence);
+        
+        System.out.println(sentence.length());
     }
 }
