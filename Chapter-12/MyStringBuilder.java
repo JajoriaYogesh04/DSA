fa8d12012c1bd5b0648b84da3f7f4417a164d3f5
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-08-10 23:22:09 +0530

    Loops in StringBuilder

--- Chapter-12/MyStringBuilder.java
+++ Chapter-12/MyStringBuilder.java
@@ -1,6 +1,6 @@
 
 public class MyStringBuilder {
-    public static void immutable(String str){
+    public static void immutable(String str){               //O(n*m)
         for(char ch='a'; ch<='z'; ch++){
             str+=ch;
             System.out.println(str);
@@ -10,16 +10,23 @@ public class MyStringBuilder {
         // String str= "Tony";
         // immutable(str);
 
-        StringBuilder sb= new StringBuilder("Tony");
-        sb.toString();              //used for objects only
-        System.out.println(sb);
+        // StringBuilder sb= new StringBuilder("Tony");
+        // sb.toString();              //used for objects only
+        // System.out.println(sb);
         
-        Character ch1= 'A';
-        ch1.toString();
-        System.out.println(ch1);
+        // Character ch1= 'A';
+        // ch1.toString();
+        // System.out.println(ch1);
 
-        Integer int1= 10;
-        int1.toString();
-        System.out.println(int1);
+        // Integer int1= 10;
+        // int1.toString();
+        // System.out.println(int1);
+
+        StringBuilder sb= new StringBuilder("");                //O(26)
+        sb.toString();
+        for(char ch='A'; ch<='Z'; ch++){
+            sb.append(ch);
+            System.out.println(sb);
+        }
     }
 }
