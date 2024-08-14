0938bb054c3b9020d810cfc541e2c5dad5561c24
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-08-15 00:28:28 +0530

    Binary XOR

--- Chapter-13/BitwiseOperators.java
+++ Chapter-13/BitwiseOperators.java
@@ -2,5 +2,6 @@ public class BitwiseOperators {
     public static void main(String args[]){
         System.out.println(5 & 6);
         System.out.println(5 | 6);
+        System.out.println(5 ^ 6);
     }
 }
