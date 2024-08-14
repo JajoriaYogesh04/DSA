3b873854cca6f0a1ceb686b2f34387f8726e6095
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-08-15 00:48:53 +0530

    Binary One's Complement

--- Chapter-13/BitwiseOperators.java
+++ Chapter-13/BitwiseOperators.java
@@ -3,5 +3,7 @@ public class BitwiseOperators {
         System.out.println(5 & 6);
         System.out.println(5 | 6);
         System.out.println(5 ^ 6);
+        // System.out.println(~5);
+        System.out.println(~0);
     }
 }
