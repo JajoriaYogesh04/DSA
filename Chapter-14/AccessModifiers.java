3f9f7b691bd150d648cde80b152ebba87c9d3f2b
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-08-19 00:16:36 +0530

    Access Modifiers

---
+++ Chapter-14/AccessModifiers.java
@@ -0,0 +1,16 @@
+public class AccessModifiers{
+    public static void main(String args[]){
+        BankAccount myAC= new BankAccount();
+        myAC.username= "krishna";
+        System.out.println(myAC.username);
+        // myAC.password= "krishna123";
+        myAC.setPassword("krishna123");
+    }
+}
+class BankAccount{
+    public String username;
+    private String password;
+    void setPassword(String newPassword){
+        password= newPassword;
+    }
+}   
