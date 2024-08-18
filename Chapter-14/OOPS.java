00a6b02fb0c5cf19c9e6e10ca930be0870938f98
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-08-18 23:53:48 +0530

    Objects and Classes

---
+++ Chapter-14/OOPS.java
@@ -0,0 +1,22 @@
+public class OOPS {
+    public static void main(String args[]){
+        Pen p1 = new Pen();
+        p1.setColor("Red");
+        System.out.println(p1.color);
+        p1.setTip(7);
+        System.out.println(p1.tip);
+        p1.tip= 4;
+        System.out.println(p1.tip);
+    }
+}
+
+class Pen{
+    String color;
+    int tip;
+    void setColor(String newColor){
+        color= newColor;
+    }
+    void setTip(int newTip){
+        tip= newTip;
+    }
+}
