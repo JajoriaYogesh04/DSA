c5764c21c031b23fa2b4d060bf1cfb13224b560c
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-08-19 16:39:58 +0530

    Method Overloading

---
+++ Chapter-14/Polymorphism.java
@@ -0,0 +1,19 @@
+public class Polymorphism {
+    public static void main(String args[]){
+        Calculator calc= new Calculator();
+        System.out.println(calc.sum(2,5));
+        System.out.println(calc.sum((float)2.2,(float)5.6));
+        System.out.println(calc.sum(2,5, 7));
+    }
+}
+class Calculator{
+    int sum(int a, int b){
+        return a+b;
+    }
+    float sum(float a, float b){
+        return a+b;
+    }
+    int sum(int a, int b, int c){
+        return a+b+c;
+    }
+}
