a61229d94b22c4eb4fb937d17f699498500f9b04
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-08-19 16:49:36 +0530

    Method Overriding

--- Chapter-14/Polymorphism.java
+++ Chapter-14/Polymorphism.java
@@ -1,9 +1,22 @@
 public class Polymorphism {
     public static void main(String args[]){
-        Calculator calc= new Calculator();
-        System.out.println(calc.sum(2,5));
-        System.out.println(calc.sum((float)2.2,(float)5.6));
-        System.out.println(calc.sum(2,5, 7));
+        // Calculator calc= new Calculator();               //Method Overoading
+        // System.out.println(calc.sum(2,5));
+        // System.out.println(calc.sum((float)2.2,(float)5.6));
+        // System.out.println(calc.sum(2,5, 7));
+
+        Dear d1= new Dear();                                //Method Overriding
+        d1.eat();
+    }
+}
+class Animal{
+    void eat(){
+        System.out.println("Eat Anything");
+    }
+}
+class Dear extends Animal{
+    void eat(){
+        System.out.println("Eat Grass");
     }
 }
 class Calculator{
