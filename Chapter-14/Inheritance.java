b1594aee6e53cd33155dce48366f60e61aa65d75
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-08-19 11:42:50 +0530

    Hybrid Inheritance

--- Chapter-14/Inheritance.java
+++ Chapter-14/Inheritance.java
@@ -9,12 +9,19 @@ public class Inheritance {
         // dobby.legs= 2;
         // System.out.println(dobby.legs);
 
-        Fish f1= new Fish();
-        Bird b1= new Bird();
-        Mammal m1= new Mammal();
-        f1.eat();
-        b1.eat();
-        m1.eat();
+        // Fish f1= new Fish();                    //Hierarchical 
+        // Bird b1= new Bird();
+        // Mammal m1= new Mammal();
+        // f1.eat();
+        // b1.eat();
+        // m1.eat();
+
+        Shark s1= new Shark();                      //Hybrid Ineritance
+        s1.shark();
+        Peacock p1= new Peacock();
+        p1.peacock();
+        Human h1= new Human();
+        h1.human();
     }
 }
 
@@ -33,14 +40,39 @@ class Mammal extends Animal{
 class Dog extends Mammal{
     String breed;
 }
+class Cat extends Mammal{
+    void cat(){
+        System.out.println("Cat");
+    }
+}
+class Human extends Mammal{
+    void human(){
+        System.out.println("Human");
+    }
+}
 class Fish extends Animal{
     int fins= 2;
     void swim(){
         System.out.println("Swims");
     }
 }
+class Shark extends Fish{
+    void shark(){
+        System.out.println("Shark");
+    }
+}
+class Tuna extends Fish{
+    void tuna(){
+        System.out.println("Tuna");
+    }
+}
 class Bird extends Animal{
     void fly(){
         System.out.println("Fly");
+    }
+}
+class Peacock extends Bird{
+    void peacock(){
+        System.out.println("Peacock");
     }
 }
