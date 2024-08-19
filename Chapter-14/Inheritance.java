a9cb38f5f3e1eef8512afb07c792006c9a3edcd9
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-08-19 11:22:38 +0530

    Hierarchical Inheritance

--- Chapter-14/Inheritance.java
+++ Chapter-14/Inheritance.java
@@ -1,13 +1,20 @@
 public class Inheritance {
     public static void main(String args[]){
-        // Fish shark= new Fish();
+        // Fish shark= new Fish();          //Single Level
         // System.out.println(shark.fins);
         // shark.eat();
 
-        Dog dobby= new Dog();
-        dobby.breath();
-        dobby.legs= 2;
-        System.out.println(dobby.legs);
+        // Dog dobby= new Dog();               //Multilevel
+        // dobby.breath();
+        // dobby.legs= 2;
+        // System.out.println(dobby.legs);
+
+        Fish f1= new Fish();
+        Bird b1= new Bird();
+        Mammal m1= new Mammal();
+        f1.eat();
+        b1.eat();
+        m1.eat();
     }
 }
 
@@ -30,5 +37,10 @@ class Fish extends Animal{
     int fins= 2;
     void swim(){
         System.out.println("Swims");
+    }
+}
+class Bird extends Animal{
+    void fly(){
+        System.out.println("Fly");
     }
 }
