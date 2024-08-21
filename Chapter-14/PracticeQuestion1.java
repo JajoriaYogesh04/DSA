94e7a2a3f76703acf5628a80b563b9e15697a076
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-08-21 22:33:08 +0530

    Modified Practice Question 1

--- Chapter-14/PracticeQuestion1.java
+++ Chapter-14/PracticeQuestion1.java
@@ -1,13 +1,31 @@
 import java.util.*;
 public class PracticeQuestion1 {
-    public static void add(Complex c1, Complex c2){
-        System.out.println("("+c1.real+" + i"+c1.img+")"+" + "+"("+c2.real+" + i"+c2.img+")"+" = "+(c1.real + c2.real)+" + i"+(c1.img + c2.img));
+    public static Complex add(Complex c1, Complex c2){
+        // System.out.println("("+c1.real+" + i"+c1.img+")"+" + "+"("+c2.real+" + i"+c2.img+")"+" = "+(c1.real + c2.real)+" + i"+(c1.img + c2.img));
+        return new Complex(c1.real + c2.real, c1.img + c2.img);
     }
-    public static void sub(Complex c1, Complex c2){
-        System.out.println("("+c1.real+" + i"+c1.img+")"+" - "+"("+c2.real+" + i"+c2.img+")"+" = "+(c1.real - c2.real)+" + i"+(c1.img - c2.img));
+    public static Complex sub(Complex c1, Complex c2){
+        // System.out.println("("+c1.real+" + i"+c1.img+")"+" - "+"("+c2.real+" + i"+c2.img+")"+" = "+(c1.real - c2.real)+" + i"+(c1.img - c2.img));
+        return new Complex(c1.real - c2.real, c1.img - c2.img);
     }
-    public static void mul(Complex c1, Complex c2){
-        System.out.println("("+c1.real+" + i"+c1.img+")"+" x "+"("+c2.real+" + i"+c2.img+")"+" = "+((c1.real * c2.real) - (c1.img * c2.img))+" + i"+((c1.real * c2.img) + (c1.img * c2.real)));
+    public static Complex mul(Complex c1, Complex c2){
+        // System.out.println("("+c1.real+" + i"+c1.img+")"+" x "+"("+c2.real+" + i"+c2.img+")"+" = "+((c1.real * c2.real) - (c1.img * c2.img))+" + i"+((c1.real * c2.img) + (c1.img * c2.real)));
+        return new Complex(((c1.real * c2.real) - (c1.img * c2.img)), ((c1.real * c2.img) + (c1.img * c2.real)));
+    }
+    public static void printComplex(Complex c){
+        // System.out.println(c.real+" + i"+c.img);
+        if(c.real==0){
+            System.out.println(c.img+"i");
+        }
+        else if(c.img==0){
+            System.out.println(c.real);
+        }
+        else if(c.img<0){
+            System.out.println(c.real+" "+c.img+"i");
+        }
+        else{
+            System.out.println(c.real+" + "+c.img+"i");
+        }
     }
     public static void main(String args[]){
         Scanner sc= new Scanner(System.in);
@@ -21,9 +39,12 @@ public class PracticeQuestion1 {
         System.out.print("Enter Imaginary Part of second number: ");
         int img2= sc.nextInt();
         Complex c2= new Complex(real2, img2);
-        add(c1, c2);
-        sub(c1, c2);
-        mul(c1, c2);
+        Complex addition= add(c1, c2);
+        printComplex(addition);
+        Complex subtraction= sub(c1, c2);
+        printComplex(subtraction);
+        Complex multiplication= mul(c1, c2);
+        printComplex(multiplication);
     }
 }
 class Complex{
