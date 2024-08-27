ee0a4956711b0323b6d30a05b2fb70db8b2d68d0
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-08-27 23:50:48 +0530

    Printing Binary strings with no consecutive 1s

---
+++ Chapter-16/BinaryStrings.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class BinaryStrings {
+    public static void binaryStrings(int n, String str, int lastPlace){
+        if(n==0){
+            System.out.println(str);
+            return;
+        }
+        binaryStrings(n-1, str+"0", 0);
+        if(lastPlace==0){
+            binaryStrings(n-1, str+"1", 1);
+        }
+    }
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter number of digits: ");
+        int n= sc.nextInt();
+        String str= "";
+        int lastPlace= 0;
+        binaryStrings(n, str, lastPlace);
+    }
+}
