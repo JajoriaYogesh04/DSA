fc15a444681a5790eea920d8d1cf722ff19ef0ac
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-09-09 23:03:16 +0530

    Modified Practice Question 2.

--- Chapter-17/PracticeQuestion2.java
+++ Chapter-17/PracticeQuestion2.java
@@ -1,7 +1,6 @@
 import java.util.*;
 public class PracticeQuestion2 {
     public static int majorityEle(int arr[], int start, int end){
-        // int mid= start+(end-start)/2;
         if(start==end){
             return arr[start];
         }
@@ -21,17 +20,12 @@ public class PracticeQuestion2 {
     }
     public static int countInRange(int arr[], int num, int si, int ei){
         int count=0;
-        for(int i=si; i<ei; i++){
+        for(int i=si; i<=ei; i++){
             if(arr[i]==num){
                 count++;
             }
         }
         return count;
-    }
-    public static void printArray(int arr[]){
-        for(int i=0; i<arr.length; i++){
-            System.out.print(arr[i]+" ");
-        }
     }
     public static void inputArray(int arr[]){
         Scanner sc= new Scanner(System.in);
