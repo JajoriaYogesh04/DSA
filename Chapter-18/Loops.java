2eca7c280161095bc30a28dc477316d305882d48
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-09-13 16:42:19 +0530

    Nested Loops 3

--- Chapter-18/Loops.java
+++ Chapter-18/Loops.java
@@ -10,8 +10,16 @@ public class Loops{
         //     }
         // }
 
-        for(int i=0; i<5; i++){                        //Nested Loops 2
-            for(int j=0; j<i; j++){
+        // for(int i=0; i<5; i++){                        //Nested Loops 2
+        //     for(int j=0; j<i; j++){
+        //         System.out.println(i+" "+j);
+        //     }
+        // }
+
+        int n=50;
+        int k=5;
+        for(int i=0; i<n; i=i+k){
+            for(int j=i+1; j<=k; j++){
                 System.out.println(i+" "+j);
             }
         }
