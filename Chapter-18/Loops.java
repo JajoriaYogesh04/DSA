d5b1f462c2d78c2815d1d6398da3b52853c865a0
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-09-13 16:08:43 +0530

    Analysing time complexity of simple loops

---
+++ Chapter-18/Loops.java
@@ -0,0 +1,7 @@
+public class Loops{
+    public static void main(String[] args) {
+        for(int i=0; i<5; i++){                 //Simple Loops
+            System.out.println(i);
+        }
+    }
+}
