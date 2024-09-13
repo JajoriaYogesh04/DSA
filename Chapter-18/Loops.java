c723be517ae10fbf08591f950e8ede48fdfe3601
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-09-13 16:15:51 +0530

    Analysing Time complexity for nested loops

--- Chapter-18/Loops.java
+++ Chapter-18/Loops.java
@@ -1,7 +1,13 @@
 public class Loops{
     public static void main(String[] args) {
-        for(int i=0; i<5; i++){                 //Simple Loops
-            System.out.println(i);
+        // for(int i=0; i<5; i++){                 //Simple Loops
+        //     System.out.println(i);
+        // }
+
+        for(int i=0; i<5; i++){                     //Nested Loops 1
+            for(int j=i+1; j<5; j++){
+                System.out.println(i+" "+j);
+            }
         }
     }
 }
