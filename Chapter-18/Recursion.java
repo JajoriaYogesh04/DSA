36945dbf138897b8ee36ad87bb7e9033277aa8df
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-09-24 20:52:37 +0530

    Time Complexity analysis for sum on n natural numbers

--- Chapter-18/Recursion.java
+++ Chapter-18/Recursion.java
@@ -1,12 +1,19 @@
-public class Factorial {
+public class Recursion {
     public static int factorial(int n){
         if(n==0){
             return 1;
         }
         return n*factorial(n-1);
     }
+    public static int sumN(int n){
+        if(n==0){
+            return 0;
+        }
+        return n+sumN(n-1);
+    }
     public static void main(String args[]){
         int n=4;
         System.out.println(factorial(n));
+        System.out.println(sumN(n));
     }
 }
