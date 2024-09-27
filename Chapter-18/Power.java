314db9b6be9b86a9fce67009d0a22cb76d785f83
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-09-27 17:22:37 +0530

    TC & SC analysis for Power Function two

--- Chapter-18/Power.java
+++ Chapter-18/Power.java
@@ -1,13 +1,24 @@
 public class Power{
-    public static int power(int base, int exp){
+    public static int powerOne(int base, int exp){
         if(exp==0){
             return 1;
         }
-        return base*power(base, exp-1);
+        return base*powerOne(base, exp-1);
     }
+    public static int powerTwo(int base, int exp){
+        if(exp==0){
+            return 1;
+        }
+        int halfPowerSquare= powerTwo(base, exp/2)*powerTwo(base, exp/2);
+        if(exp%2!=0){
+            return halfPowerSquare*base;
+        }
+        return halfPowerSquare;
+    }  
     public static void main(String args[]){
         int base= 2;
         int exp= 10;
-        System.out.println(power(base, exp));
+        System.out.println(powerOne(base, exp));
+        System.out.println(powerTwo(base, exp));
     }
 }
