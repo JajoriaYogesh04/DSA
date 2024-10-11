ae2038b66049b79a7323d907b21ead0408528563
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-10-11 21:52:11 +0530

    Total number of ways in NQueens

--- Chapter-19/NQueens.java
+++ Chapter-19/NQueens.java
@@ -1,28 +1,30 @@
+import java.util.*;
 public class NQueens {
     public static boolean isSafe(String board[][], int row, int col){
-        boolean ret= true;
+        // boolean ret= true;
         //vertical up
         for(int i=row-1; i>=0; i--){
             if(board[i][col]=="Q"){
-                ret= false;
+                return false;
             }
         }
         //diagonal left
         for(int i=row-1, j=col-1; i>=0 && j>=0; i--, j--){
             if(board[i][j]=="Q"){
-                ret= false;
+                return false;
             }
         }
         //diagonal right
         for(int i=row-1, j=col+1; i>=0 && j<board[0].length; i--, j++){
             if(board[i][j]=="Q"){
-                ret= false;
+                return false;
             }
         }
-        return ret;
+        return true;
     }
     public static void nQueens(String board[][], int row){
         if(row==board.length){
+            count++;
             printBoard(board);
             return;
         }
@@ -32,7 +34,6 @@ public class NQueens {
                 nQueens(board, row+1);                      //function call
                 board[row][j]= "X";                         //backtrack
             }
-            
         }
     }
     public static void printBoard(String board[][]){
@@ -44,8 +45,11 @@ public class NQueens {
         }
         System.out.println();
     }
+    static int count= 0;
     public static void main(String args[]){
-        int n= 5;
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter N: ");
+        int n= sc.nextInt();
         String board[][]= new String[n][n];
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
@@ -53,5 +57,6 @@ public class NQueens {
             }
         }
         nQueens(board, 0);
+        System.out.println("Number of ways: "+count);
     }
 }
