0fad146ed6021715abf3a70c25818a41539275ef
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-10-17 19:28:42 +0530

    LC: Palindrome Number

--- CodingNinja/PalindromeNumber.java
+++ CodingNinja/PalindromeNumber.java
@@ -14,3 +14,23 @@ public class Solution {
         return false;
     }
 }
+
+
+//LEETCODE 
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0 || (x != 0 && x % 10 == 0)) {
+            return false;
+        }
+
+        int reversed = 0;
+        int original = x;
+
+        while (x > reversed) {
+            reversed = reversed * 10 + x % 10;
+            x /= 10;
+        }
+
+        return (x == reversed) || (x == reversed / 10);
+    }
+}
