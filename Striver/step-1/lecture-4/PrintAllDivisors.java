a7a5e13ee5c43a2365ff0bca0d9616a4f4c6c7d1
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-10-18 20:54:03 +0530

    Striver: Sum of all Divisors from 1 to N

--- Striver/step-1/lecture-4/PrintAllDivisors.java
+++ Striver/step-1/lecture-4/PrintAllDivisors.java
@@ -1,3 +1,4 @@
+//CodingNinja
 public class Solution {
     public static int sumOfAllDivisors(int n){
         int sumArr[]= new int[n+1];
@@ -8,6 +9,47 @@ public class Solution {
         }
         int totalSum= 0;
         for(int i=1; i<=n; i++){
+            totalSum+=sumArr[i];
+        }
+        return totalSum;
+    }
+}
+
+//GFG
+//GFG
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            int N=sc.nextInt();
+            Solution ob = new Solution();
+            long ans  = ob.sumOfDivisors(N);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution{
+    static long sumOfDivisors(int N){
+        long sumArr[]= new long[N+1];
+        for(int i= 1; i<=N; i++){
+            sumArr[i]= (N/i)*i;
+        }
+        long totalSum= 0;
+        for(int i=1; i<=N; i++){
             totalSum+=sumArr[i];
         }
         return totalSum;
