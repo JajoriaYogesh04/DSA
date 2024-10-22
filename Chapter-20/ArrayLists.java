d67fc5f767112c61d26cc1582712b9325403ebf6
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-10-22 16:44:38 +0530

    Size of ArrayList

--- Chapter-20/ArrayLists.java
+++ Chapter-20/ArrayLists.java
@@ -5,24 +5,38 @@ public class ArrayLists {
         ArrayList<Integer> list1 = new ArrayList<>();
         ArrayList<Boolean> list2 = new ArrayList<>();
         ArrayList<String> list3 = new ArrayList<>();
-        //ADD
-        list1.add(1);
-        list1.add(2);
-        list1.add(3);
-        list1.add(1, 4);
-        System.out.println(list1);
-        //GET
-        int element= list1.get(1);
-        System.out.println(element);
-        //REMOVE;
-        list1.remove(2);
-        System.out.println(list1);
-        //SET
-        list1.set(2, 5);
-        System.out.println(list1);
-        //True False
-        System.out.println(list1.contains(1));
-        System.out.println(list1.contains(3));
         
+        //Operations
+        // //ADD
+        // list1.add(1);
+        // list1.add(2);
+        // list1.add(3);
+        // list1.add(1, 4);
+        // System.out.println(list1);
+        // //GET
+        // int element= list1.get(1);
+        // System.out.println(element);
+        // //REMOVE;
+        // list1.remove(2);
+        // System.out.println(list1);
+        // //SET
+        // list1.set(2, 5);
+        // System.out.println(list1);
+        // //True False
+        // System.out.println(list1.contains(1));
+        // System.out.println(list1.contains(3));
+        
+        //Size
+        ArrayList<Integer> list4= new ArrayList<>();
+        list4.add(1);
+        list4.add(2);
+        list4.add(3);
+        list4.add(4);
+        list4.add(5);
+        System.out.println(list4);
+        System.out.println(list4.size());
+        for(int i=0; i<list4.size(); i++){
+            System.out.print(list4.get(i)+" ");
+        }
     }
 }
