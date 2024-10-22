75eb9c5b6fd0359cc3b9f52f3a80f06704a3ea61
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-10-23 02:42:06 +0530

    Container With Maximum Water Optimized

--- Chapter-20/ContainerWithMaxWater.java
+++ Chapter-20/ContainerWithMaxWater.java
@@ -1,24 +1,30 @@
 import java.util.ArrayList;
 public class ContainerWithMaxWater {
-    public static int minimum(int a, int b){
-        if(a<=b){
-            return a;
-        }
-        else{
-            return b;
-        }
-    }
+    // public static int containerWithMaxWater(ArrayList<Integer> height){
+    //     int volume= Integer.MIN_VALUE;
+    //     for(int i=0; i<height.size()-1; i++){
+    //         for(int j=2; j<height.size(); j++){
+    //             int start= height.get(i);
+    //             int end= height.get(j);
+    //             int vol= (j-i)*Math.min(start, end)*1;
+    //             volume= Math.max(vol, volume);
+    //         }
+    //     }
+    //     return volume;
+    // }
     public static int containerWithMaxWater(ArrayList<Integer> height){
+        int i= 0;
+        int j= height.size()-1;
         int volume= Integer.MIN_VALUE;
-        for(int i=0; i<height.size()-1; i++){
-            for(int j=2; j<height.size(); j++){
-                int start= height.get(i);
-                int end= height.get(j);
-                int vol= (j-i)*minimum(start, end)*1;
-                if(vol>volume){
-                    volume= vol;
-                }
+        while (i<j) {
+            if(height.get(i)<=height.get(j)){
+                i++;
             }
+            else{
+                j--;
+            }
+            int vol= (j-1)*(Math.min(height.get(i), height.get(j)));
+            volume= Math.max(vol, volume);
         }
         return volume;
     }
