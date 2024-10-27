88c8c383b7feeefbabd44a4cb2fc8b739b876bd0
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-10-27 10:42:01 +0530

    Pair Sum(Two Pointer)

--- Chapter-20/PairSum.java
+++ Chapter-20/PairSum.java
@@ -1,11 +1,27 @@
 import java.util.*;
 public class PairSum {
-    public static boolean pairSum(ArrayList<Integer>list, int n){
+    public static boolean pairSum1(ArrayList<Integer>list, int n){
         for(int i=0; i<list.size()-1; i++){
             for(int j=i+1; j<list.size(); j++){
                 if((list.get(i)+list.get(j)==n)){
                     return true;
                 }
+            }
+        }
+        return false;
+    }
+    public static boolean pairSum2(ArrayList<Integer> list, int n){
+        int li= 0;
+        int ri= list.size()-1;
+        while(li<ri){
+            if(list.get(li)+list.get(ri)==n){
+                return true;
+            }
+            else if(list.get(li)+list.get(ri)<n){
+                li++;
+            }
+            else if(list.get(li)+list.get(ri)>n){
+                ri--;
             }
         }
         return false;
@@ -20,6 +36,7 @@ public class PairSum {
         list.add(6);
         System.out.println(list);
         int target= 5;
-        System.out.println(pairSum(list, target));
+        // System.out.println(pairSum1(list, target));
+        System.out.println(pairSum2(list, target));
     }
 }
