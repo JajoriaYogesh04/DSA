057422e86e00e4c21f8661d25d018efd9dfa2a13
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-11-02 09:41:20 +0530

    CN: Remove Duplicates from sorted Array

---
+++ CodingNinja/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public static int removeDuplicates(int[] arr,int n) {
+        // Write your code here.
+        int count= 1;
+        if(n==1){
+            return count;
+        }
+        for(int i=1; i<arr.length; i++){
+            if(arr[i]>arr[i-1]){
+                count++;
+            }
+        }
+        return count;
+    }
+}
