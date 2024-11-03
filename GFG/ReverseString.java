52849881b21bd77c48d5f474f30e5a1062fc6cb8
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-11-03 22:38:56 +0530

    GFG: Reverse String

---
+++ GFG/ReverseString.java
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Driver {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+
+        while (t-- > 0) {
+            String str = read.readLine();
+            System.out.println(new Reverse().reverseString(str));
+
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Reverse {
+    // Complete the function
+    // str: input string
+    public static String reverseString(String s) {
+        // Reverse the string str
+        String rev= "";
+        for(int i=0; i<s.length(); i++){
+            rev+=s.charAt(s.length()-1-i);
+        }
+        return rev;
+    }
+}
