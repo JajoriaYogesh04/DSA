808c0f00f20c5525bbf07b13e40b5cb227cb6608
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-11-06 01:56:16 +0530

    Remove Last in LL

--- Chapter-21/LinkedList.java
+++ Chapter-21/LinkedList.java
@@ -63,7 +63,7 @@ public class LinkedList{
         temp.next= newNode;
     }
     public static int removeHead(){
-        if(head.next==null){
+        if(size==0){
             return Integer.MIN_VALUE;
         }
         else if(size==1){
@@ -77,17 +77,44 @@ public class LinkedList{
         size--;
         return val;
     }
-    // public static void remove(int idx){
-    //     Node temp= head;
-    //     int i=0;
-    //     while(i<idx-1){
-    //         temp= temp.next;
-    //         i++;
-    //     }
-    //     Node rem= temp.next;
-    //     temp.next= rem.next;
-    //     rem.next= null;
-    // }
+    public static int removeLast(){
+        if(size==0){
+            return Integer.MIN_VALUE;
+        }
+        else if(size==1){
+            int val= head.data;
+            head= tail= null;
+            size=0;
+            return val;
+        }
+        Node temp= head;
+        int i= 0;
+        while(i<size-2){
+            temp= temp.next;
+            i++;
+        }
+        int val= temp.next.data;                //tail.data
+        temp.next= null;
+        tail= temp;
+        size--;
+        return val;
+    }
+    public static int remove(int idx){
+        if(idx==0){
+            return removeHead();
+        }
+        Node temp= head;
+        int i=0;
+        while(i<idx-1){
+            temp= temp.next;
+            i++;
+        }
+        Node rem= temp.next;
+        temp.next= rem.next;
+        rem.next= null;
+        size--;
+        return rem.data;
+    }
     public static void main(String args[]){
         LinkedList ll= new LinkedList();
         print();
@@ -105,11 +132,15 @@ public class LinkedList{
         print();
         ll.addMiddle(0, 0);             //Add Middle do not work for add first
         print();
-        System.out.println(size);
+        System.out.println("Size: "+size);
         // ll.remove(3);
         // print();
-        System.out.println(ll.removeHead());
+        System.out.println("Removed: "+ll.removeHead());
         print();
-        System.out.println(size);
+        // System.out.println(size);
+        System.out.println("Removed: "+ll.removeLast());
+        print();
+        System.out.println("Removed: "+ll.remove(2));
+        print();
     }
 }
