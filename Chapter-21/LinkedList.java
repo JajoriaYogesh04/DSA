1b4da91dc61fe82b5e259bc9c602d27f4b71cecf
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-11-06 17:24:25 +0530

    iterative search in ll

--- Chapter-21/LinkedList.java
+++ Chapter-21/LinkedList.java
@@ -115,6 +115,18 @@ public class LinkedList{
         size--;
         return rem.data;
     }
+    public static int itrSearch(int key){
+        Node temp= head;
+        int i=0;
+        while(temp!=null){
+            if(temp.data==key){
+                return i;
+            }
+            temp= temp.next;
+            i++;
+        }
+        return -1;
+    }
     public static void main(String args[]){
         LinkedList ll= new LinkedList();
         print();
@@ -142,5 +154,6 @@ public class LinkedList{
         print();
         System.out.println("Removed: "+ll.remove(2));
         print();
+        System.out.println(ll.itrSearch(3));
     }
 }
