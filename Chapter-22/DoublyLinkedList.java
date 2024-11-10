a3663578b4d10bd5da08b6f9e1ca4f6649d06e50
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-11-10 16:20:11 +0530

    add last in dll

--- Chapter-22/DoublyLinkedList.java
+++ Chapter-22/DoublyLinkedList.java
@@ -23,6 +23,17 @@ public class DoublyLinkedList{
         head.prev= newNode;
         head= newNode;
     }
+    public void addLast(int data){
+        Node newNode= new Node(data);
+        size++;
+        if(head==null){
+            head= tail= newNode;
+            return;
+        }
+        tail.next= newNode;
+        newNode.prev= tail;
+        tail= newNode;
+    }
     public void print(){
         if(head==null){
             System.out.println("null");
@@ -43,6 +54,13 @@ public class DoublyLinkedList{
         dll.addFirst(2);
         dll.print();
         dll.addFirst(3);
+        dll.print();
+        System.out.println("size: "+size);
+        dll.addLast(4);
+        dll.print();
+        dll.addLast(5);
+        dll.print();
+        dll.addLast(6);
         dll.print();
         System.out.println("size: "+size);
     }
