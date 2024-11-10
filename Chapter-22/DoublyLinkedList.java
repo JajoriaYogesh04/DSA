7ce5fce3a6c5fc9bf54eb0adfc0c9f2134e5bf63
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-11-10 16:10:21 +0530

    add first in dll

--- Chapter-22/DoublyLinkedList.java
+++ Chapter-22/DoublyLinkedList.java
@@ -12,7 +12,38 @@ public class DoublyLinkedList{
     public Node head;
     public Node tail;
     public static int size;
+    public void addFirst(int data){
+        Node newNode = new Node(data);
+        size++;
+        if(head==null){
+            head= tail= newNode;
+            return;
+        }
+        newNode.next= head;
+        head.prev= newNode;
+        head= newNode;
+    }
+    public void print(){
+        if(head==null){
+            System.out.println("null");
+            return;
+        }
+        Node temp= head;
+        while(temp!=null){
+            System.out.print(temp.data+"<->");
+            temp= temp.next;
+        }
+        System.out.println("null");
+    }
     public static void main(String args[]){
         DoublyLinkedList dll= new DoublyLinkedList();
+        dll.print();
+        dll.addFirst(1);
+        dll.print();
+        dll.addFirst(2);
+        dll.print();
+        dll.addFirst(3);
+        dll.print();
+        System.out.println("size: "+size);
     }
 }
