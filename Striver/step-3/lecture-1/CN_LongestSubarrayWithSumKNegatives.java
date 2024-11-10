94b7546a8886efd6bbb29054dabb9de86f312a45
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-11-10 19:47:42 +0530

    striver: cn: longest subarray with sum k negatives

---
+++ Striver/step-3/lecture-1/CN_LongestSubarrayWithSumKNegatives.java
@@ -0,0 +1,26 @@
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+	public static int getLongestSubarray(int []nums, int k) {
+		// Write your code here.
+		int n= nums.length;
+		Map<Integer, Integer> preSumMap= new HashMap<>();
+		int sum= 0;
+		int maxLen= 0;
+		for(int i=0; i<n; i++){
+			sum+=nums[i];
+			if(sum==k){
+				maxLen= Math.max(maxLen, i+1);
+			}
+			int rem= sum-k;
+			if(preSumMap.containsKey(rem)){
+				int len= i-preSumMap.get(rem);
+				maxLen= Math.max(maxLen, len);
+			}
+			if(!preSumMap.containsKey(sum)){
+				preSumMap.put(sum, i);
+			}
+		}
+		return maxLen;
+	}
+}
