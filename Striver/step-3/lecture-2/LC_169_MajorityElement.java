17e855a7835ed064a6ddbb6f641d802ead1a4afb
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-11-13 19:27:33 +0530

    striver: lc: 169: majority element(>n/2 times)

---
+++ Striver/step-3/lecture-2/LC_169_MajorityElement.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int ele= nums[0];
+        int count= 0;
+        for(int i=0; i<nums.length; i++){
+            if(count==0){
+                ele= nums[i];
+            }
+            if(ele==nums[i]){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+        count= 0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]==ele){
+                count++;
+            }
+            if(count>nums.length/2){
+                return ele;
+            }
+        }
+        return -1;
+    }
+}
