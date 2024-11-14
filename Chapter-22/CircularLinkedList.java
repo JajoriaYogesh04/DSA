3644e298b4512ba5e96cea6dd2a3f4aa81ae0230
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-11-14 13:34:00 +0530

    add last in cll

--- Chapter-22/CircularLinkedList.java
+++ Chapter-22/CircularLinkedList.java
@@ -2,41 +2,59 @@ public class CircularLinkedList{
     public class Node{
         int data;
         Node next;
-        public Node(int data){
-            this.data= data;
-        }
     }
-    public Node head;
-    public Node tail;
-    public void addFirst(int data){
-        Node newNode= new Node(data);
-        if(head== null){
-            head= tail= newNode;
-            tail.next= head;
+    public Node last;
+    public void addEmpty(int data){
+        if(last!=null){
             return;
         }
-        newNode.next= head;             //newNode.next= tail.next;
-        head= newNode;
-        tail.next= head;
+        Node newNode= new Node();
+        newNode.data= data;
+        last= newNode;
+        newNode.next= last;
+    }
+    public void addFirst(int data){
+        if(last==null){
+            addEmpty(data);
+            return;
+        }
+        Node newNode= new Node();
+        newNode.data= data;
+        newNode.next= last.next;             //newNode.next= tail.next;
+        last.next= newNode;
+    }
+    public void addLast(int data){
+        if(last==null){
+            addEmpty(data);
+            return;
+        }
+        Node newNode= new Node();
+        newNode.data= data;
+        newNode.next= last.next;
+        last.next= newNode;
+        last= newNode;
     }
     public void print(){
-        Node temp= head;
-        if(head==null){
+        if(last==null){
             System.out.println("null");
             return;
         }
+        Node temp= last.next;
         do{
             System.out.print(temp.data+"->");
             temp= temp.next;
-        }while(temp!=head);
+        }while(temp!=last.next);
         System.out.println("circular");
     }
     public static void main(String args[]){
         CircularLinkedList cll= new CircularLinkedList();
         cll.print();
+        cll.addEmpty(0);
         cll.addFirst(1);
         cll.addFirst(2);
         cll.addFirst(3);
+        cll.addLast(4);
+        cll.addLast(5);
         cll.print();
     }
 }
