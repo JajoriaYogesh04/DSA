df2b84692edde2320efcaa87ecad5fafc88ed00d
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-11-21 18:17:38 +0530

    gfg: factorial

---
+++ GFG/Factorial.java
@@ -0,0 +1,11 @@
+
+
+class Solution{
+    static long factorial(int N){
+        // code here
+        if(N==0 || N==1){
+            return (long)1;
+        }
+        return N*factorial(N-1);
+    }
+}
