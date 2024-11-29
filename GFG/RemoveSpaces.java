c2b745b0f4b0c64308decb2eb8d0caf75f825d49
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-11-29 23:31:59 +0530

    gfg: remove spaces

---
+++ GFG/RemoveSpaces.java
@@ -0,0 +1,13 @@
+
+
+//User function Template for Java
+class Solution
+{
+   
+    String modify(String S)
+    {
+        // your code here
+        S=S.replace(" ", "");
+        return S;
+    }
+}
