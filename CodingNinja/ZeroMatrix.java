7e0574710e22e3210c28dc8bc4dd148da295220c
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-11-30 21:40:19 +0530

    coding ninja

---
+++ CodingNinja/ZeroMatrix.java
@@ -0,0 +1,31 @@
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+    public static ArrayList<ArrayList<Integer>> zeroMatrix(ArrayList<ArrayList<Integer>> matrix, Integer n, Integer m) {
+    	// Write your code here.
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(matrix.get(i).get(j)==0){
+                    for(int col=0; col<m; col++){
+                        if(matrix.get(i).get(j)!=0){
+                            matrix.get(i).set(col, -1);
+                        } 
+                    }
+                    for(int row=0; row<n; row++){
+                        if(matrix.get(i).get(j)!=0){
+                            matrix.get(j).set(row, -1);
+                        }
+                    }
+                }
+            }
+        }
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(matrix.get(i).get(j)==-1){
+                    matrix.get(i).set(j, 0);
+                }
+            }
+        }
+        return matrix;
+    }
+}
