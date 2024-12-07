f0bc7f70b13d9e3aea159b34985a5070b4bc304b
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-12-07 10:18:32 +0530

    practice question 1: palindrome linked list

---
+++ Chapter-24/PalindromeLinkedList.java
@@ -0,0 +1,92 @@
+import java.util.*;
+public class PalindromeLinkedList {
+    public static class Node{
+        char data;
+        Node next;
+        public Node(char data){
+            this.data= data;
+            this.next= null;
+        }
+    }
+    public static void printLinkedList(Node head){
+        if(head==null){
+            System.out.println("null");
+            return;
+        }
+        Node temp= head;
+        while(temp!=null){
+            System.out.print(temp.data+"->");
+            temp= temp.next;
+        }
+        System.out.println("null");
+        return;
+    }
+    public static void printStack(Stack<Character> s){
+        if(s.isEmpty()){
+            return;
+        }
+        char top= s.pop();
+        System.out.println(top);
+        printStack(s);
+        s.push(top);
+    }
+    public static void pushAtBottom(Stack<Character> s, char ch){
+        if(s.isEmpty()){
+            s.push(ch);
+            return;
+        }
+        char top= s.pop();
+        pushAtBottom(s, ch);
+        s.push(top);
+    }
+    public static void reverseStack(Stack<Character> s){
+        if(s.isEmpty()){
+            return;
+        }
+        char top= s.pop();
+        reverseStack(s);
+        pushAtBottom(s, top);
+    }
+    public static Stack<Character> createStack(Node head){
+        Stack<Character> s= new Stack<>();
+        Node temp= head;
+        while(temp!=null){
+            s.push(temp.data);
+            temp= temp.next;
+        }
+        return s;
+    }
+    public static boolean isPalindromeLinkedList(Node head){
+        Stack<Character> word= createStack(head);
+        System.out.println("word stack:-");
+        printStack(word);
+
+        // Stack<Character> revword= word;
+        Stack<Character> revword= createStack(head);
+        reverseStack(revword);
+        System.out.println("reverse word stack:-");
+        printStack(word);
+
+        while(!word.isEmpty() && !revword.isEmpty()){
+            if(word.pop()!=revword.pop()){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String args[]){
+        Node head= new Node('A');
+        head.next= new Node('B');
+        head.next.next= new Node('C');
+        head.next.next.next= new Node('D');
+        head.next.next.next.next= new Node('E');
+        printLinkedList(head);
+        boolean isPalindrome= isPalindromeLinkedList(head);
+        if(isPalindrome){
+            System.out.println("YES It is Palindrome");
+        }
+        else{
+            System.out.println("NO It not Palindrome");
+        }
+    }
+}
