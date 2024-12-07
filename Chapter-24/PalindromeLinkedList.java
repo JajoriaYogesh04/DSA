7b50e1d6f910a2538b9440f3421c6b6f5fe5a5c3
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-12-07 11:24:17 +0530

    palindrome linked list modified

--- Chapter-24/PalindromeLinkedList.java
+++ Chapter-24/PalindromeLinkedList.java
@@ -30,56 +30,72 @@ public class PalindromeLinkedList {
         printStack(s);
         s.push(top);
     }
-    public static void pushAtBottom(Stack<Character> s, char ch){
-        if(s.isEmpty()){
-            s.push(ch);
-            return;
-        }
-        char top= s.pop();
-        pushAtBottom(s, ch);
-        s.push(top);
-    }
-    public static void reverseStack(Stack<Character> s){
-        if(s.isEmpty()){
-            return;
-        }
-        char top= s.pop();
-        reverseStack(s);
-        pushAtBottom(s, top);
-    }
-    public static Stack<Character> createStack(Node head){
-        Stack<Character> s= new Stack<>();
-        Node temp= head;
-        while(temp!=null){
-            s.push(temp.data);
-            temp= temp.next;
-        }
-        return s;
-    }
+    // public static void pushAtBottom(Stack<Character> s, char ch){
+    //     if(s.isEmpty()){
+    //         s.push(ch);
+    //         return;
+    //     }
+    //     char top= s.pop();
+    //     pushAtBottom(s, ch);
+    //     s.push(top);
+    // }
+    // public static void reverseStack(Stack<Character> s){
+    //     if(s.isEmpty()){
+    //         return;
+    //     }
+    //     char top= s.pop();
+    //     reverseStack(s);
+    //     pushAtBottom(s, top);
+    // }
+    // public static Stack<Character> createStack(Node head){
+    //     Stack<Character> s= new Stack<>();
+    //     Node temp= head;
+    //     while(temp!=null){
+    //         s.push(temp.data);
+    //         temp= temp.next;
+    //     }
+    //     return s;
+    // }
+    // public static boolean isPalindromeLinkedList(Node head){
+    //     Stack<Character> word= createStack(head);
+    //     System.out.println("word stack:-");
+    //     printStack(word);
+    //     Stack<Character> revword= createStack(head);
+    //     reverseStack(revword);
+    //     System.out.println("reverse word stack:-");
+    //     printStack(word);
+
+    //     while(!word.isEmpty() && !revword.isEmpty()){
+    //         if(word.pop()!=revword.pop()){
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
     public static boolean isPalindromeLinkedList(Node head){
-        Stack<Character> word= createStack(head);
+        Stack<Character> word= new Stack<>();
+        Node temp1= head;
+        while(temp1!=null){
+            word.push(temp1.data);
+            temp1= temp1.next;
+        }
         System.out.println("word stack:-");
         printStack(word);
-
-        // Stack<Character> revword= word;
-        Stack<Character> revword= createStack(head);
-        reverseStack(revword);
-        System.out.println("reverse word stack:-");
-        printStack(word);
-
-        while(!word.isEmpty() && !revword.isEmpty()){
-            if(word.pop()!=revword.pop()){
+        Node temp=head;
+        while(!word.isEmpty() && temp!=null){
+            if(word.pop()!=temp.data){
                 return false;
             }
+            temp= temp.next;
         }
         return true;
     }
     public static void main(String args[]){
-        Node head= new Node('A');
-        head.next= new Node('B');
-        head.next.next= new Node('C');
-        head.next.next.next= new Node('D');
-        head.next.next.next.next= new Node('E');
+        Node head= new Node('N');
+        head.next= new Node('A');
+        head.next.next= new Node('M');
+        head.next.next.next= new Node('A');
+        head.next.next.next.next= new Node('N');
         printLinkedList(head);
         boolean isPalindrome= isPalindromeLinkedList(head);
         if(isPalindrome){
