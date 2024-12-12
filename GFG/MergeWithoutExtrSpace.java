65eec6b604b43a247b018bd4c2f13c802586d722
JajoriaYogesh04 <yogeshjajoria04@gmail.com>
2024-12-12 08:14:41 +0530

    gfg: merge withour extra space gap approach

--- GFG/MergeWithoutExtrSpace.java
+++ GFG/MergeWithoutExtrSpace.java
@@ -1,28 +1,74 @@
+//OPTIMAL 1
 
+// // User function Template for Java
+
+// class Solution {
+//     // Function to merge the arrays.
+//     public void mergeArrays(int a[], int b[]) {
+//         // code here
+//         int m= a.length;
+//         int n= b.length;
+//         int left= m-1;
+//         int right= 0;
+//         while(left>=0 && right<n){
+//             if(a[left]>b[right]){
+//                 int temp= a[left];
+//                 a[left]= b[right];
+//                 b[right]= temp;
+//                 left--;
+//                 right++;
+//             }
+//             else{
+//                 break;
+//             }
+//         }
+//         Arrays.sort(a);
+//         Arrays.sort(b);
+//     }
+// }
+
+
+
+
+//OPTIMAL 2
 
 // User function Template for Java
 
 class Solution {
     // Function to merge the arrays.
+    private void swapArray(int a[], int b[], int m, int n){
+        if(a[m]>b[n]){
+            int temp= a[m];
+            a[m]= b[n];
+            b[n]= temp;   
+        }
+    }
     public void mergeArrays(int a[], int b[]) {
         // code here
         int m= a.length;
         int n= b.length;
-        int left= m-1;
-        int right= 0;
-        while(left>=0 && right<n){
-            if(a[left]>b[right]){
-                int temp= a[left];
-                a[left]= b[right];
-                b[right]= temp;
-                left--;
-                right++;
+        int len= m+n;
+        int gap= (len/2)+(len%2);
+        while(gap>0){
+            int left= 0;
+            int right= left+gap;
+            while(right<len){
+                if(left<m && right>=m){
+                    swapArray(a, b, left, right-m);
+                }
+                else if(left>=m){
+                    swapArray(b, b, left-m, right-m);
+                }
+                else{
+                    swapArray(a, a, left, right);
+                }
+                left++;
+                right++;   
             }
-            else{
+            if(gap==1){
                 break;
             }
+            gap= (gap/2)+(gap%2);
         }
-        Arrays.sort(a);
-        Arrays.sort(b);
     }
 }
