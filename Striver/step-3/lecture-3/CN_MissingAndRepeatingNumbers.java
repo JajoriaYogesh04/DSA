d7d47fc750d3c43c2438a173eff5f6491c5e8287
JajoriaYogesh04 <yogeshjajoria04@gmail.com>
2024-12-13 08:12:40 +0530

    striver: cn: find the repeating and missing numbers optimal approach

--- Striver/step-3/lecture-3/CN_MissingAndRepeatingNumbers.java
+++ Striver/step-3/lecture-3/CN_MissingAndRepeatingNumbers.java
@@ -29,27 +29,50 @@
 
 //BETTER APPROACH
 
+// public class Solution {
+//     public static int[] findMissingRepeatingNumbers(int []a) {
+//         // Write your code here
+//         int n= a.length;
+//         int arr[]= new int[n+1];
+//         for(int i=0; i<n; i++){
+//             arr[a[i]]++;
+//         }
+//         int rep= -1;
+//         int mis= -1;
+//         for(int i=1; i<=n; i++){
+//             if(arr[i]==2){
+//                 rep= i;
+//             }
+//             else if(arr[i]==0){
+//                 mis= i;
+//             }
+//             if(rep!=-1 && mis!=-1){
+//                 break;
+//             }
+//         }
+//         return new int[]{rep, mis};
+//     }
+// }
+
+
+//OPTIMAL APPROACH 1
+
 public class Solution {
     public static int[] findMissingRepeatingNumbers(int []a) {
         // Write your code here
-        int n= a.length;
-        int arr[]= new int[n+1];
+        long n= a.length;
+        long s= 0;
+        long s2= 0;
         for(int i=0; i<n; i++){
-            arr[a[i]]++;
+            s+=a[i];
+            s2+=(long)a[i]*(long)a[i];
         }
-        int rep= -1;
-        int mis= -1;
-        for(int i=1; i<=n; i++){
-            if(arr[i]==2){
-                rep= i;
-            }
-            else if(arr[i]==0){
-                mis= i;
-            }
-            if(rep!=-1 && mis!=-1){
-                break;
-            }
-        }
-        return new int[]{rep, mis};
+        long sn= (n*(n+1))/2;
+        long s2n= (n*(n+1)*(2*n+1))/6;
+        long xsuby= s-sn;
+        long xaddy= (s2-s2n)/xsuby;
+        long x= (xsuby+xaddy)/2;
+        long y= x-xsuby;
+        return new int[]{(int)x,(int)y};
     }
 }
