76d7c1153879a122bfe176bd1d20bc4c0ce0ef69
JajoriaYogesh04 <yogeshjajoria04@gmail.com>
2024-12-13 08:59:19 +0530

    striver: cn: find the missing and repeating numbers optimal xor approach

--- Striver/step-3/lecture-3/CN_MissingAndRepeatingNumbers.java
+++ Striver/step-3/lecture-3/CN_MissingAndRepeatingNumbers.java
@@ -57,22 +57,80 @@
 
 //OPTIMAL APPROACH 1
 
+// public class Solution {
+//     public static int[] findMissingRepeatingNumbers(int []a) {
+//         // Write your code here
+//         long n= a.length;
+//         long s= 0;
+//         long s2= 0;
+//         for(int i=0; i<n; i++){
+//             s+=a[i];
+//             s2+=(long)a[i]*(long)a[i];
+//         }
+//         long sn= (n*(n+1))/2;
+//         long s2n= (n*(n+1)*(2*n+1))/6;
+//         long xsuby= s-sn;
+//         long xaddy= (s2-s2n)/xsuby;
+//         long x= (xsuby+xaddy)/2;
+//         long y= x-xsuby;
+//         return new int[]{(int)x,(int)y};
+//     }
+// }
+
+
+//OPTIMAL APPROACH 2
+
 public class Solution {
     public static int[] findMissingRepeatingNumbers(int []a) {
         // Write your code here
-        long n= a.length;
-        long s= 0;
-        long s2= 0;
-        for(int i=0; i<n; i++){
-            s+=a[i];
-            s2+=(long)a[i]*(long)a[i];
+        int n= a.length;
+        int xr=0;
+        for(int i=1; i<=n; i++){
+            xr^=a[i-1];
+            xr^=i;
         }
-        long sn= (n*(n+1))/2;
-        long s2n= (n*(n+1)*(2*n+1))/6;
-        long xsuby= s-sn;
-        long xaddy= (s2-s2n)/xsuby;
-        long x= (xsuby+xaddy)/2;
-        long y= x-xsuby;
-        return new int[]{(int)x,(int)y};
+        int bitNo=0;
+        while(true){
+            if((xr & (1<<bitNo)) != 0){
+                break;
+            }
+            bitNo++;
+        }
+        int zero=0;
+        int one= 0;
+        for(int i=0; i<n; i++){
+            if((a[i] & (1<<bitNo))==0){
+                zero^=a[i];
+            }
+            else{
+                one^=a[i];
+            }
+        }
+        for(int i=1; i<=n; i++){
+            if((i & (1<<bitNo))==0){
+                zero^=i;
+            }
+            else{
+                one^=i;
+            }
+        }
+        int count=0;
+        for(int i=0; i<n; i++){
+            if(a[i]==zero){
+                count++;
+            }
+        }
+        int x=-1;
+        int y=-1;
+        if(count==2){
+            x= zero;
+            y= one;
+        }
+        else if(count==0){
+            x= one;
+            y= zero;
+        }
+        return new int[]{x, y};
+
     }
 }
