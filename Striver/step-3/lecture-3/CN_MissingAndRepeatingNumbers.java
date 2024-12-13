816ec599b443a41c1929722fec0210e5ded3c40d
JajoriaYogesh04 <yogeshjajoria04@gmail.com>
2024-12-13 07:19:52 +0530

    striver: cn: find the missing and repeacting numbers better approach

--- Striver/step-3/lecture-3/CN_MissingAndRepeatingNumbers.java
+++ Striver/step-3/lecture-3/CN_MissingAndRepeatingNumbers.java
@@ -1,22 +1,52 @@
+//BRUTE FORCE
+
+// public class Solution {
+//     public static int[] findMissingRepeatingNumbers(int []a) {
+//         // Write your code here
+//         int rep= -1;
+//         int mis= -1;
+//         for(int i=1; i<=a.length; i++){
+//             int count= 0;
+//             for(int j=0; j<a.length; j++){
+//                 if(a[j]==i){
+//                     count++;
+//                 }
+//             }
+//             if(count==2){
+//                 rep= i;
+//             }
+//             else if(count==0){
+//                 mis= i;
+//             }
+//             if(count==2 && mis==0){
+//                 break;
+//             }
+//         }
+//         return new int[]{rep, mis};
+//     }
+// }
+
+
+//BETTER APPROACH
+
 public class Solution {
     public static int[] findMissingRepeatingNumbers(int []a) {
         // Write your code here
+        int n= a.length;
+        int arr[]= new int[n+1];
+        for(int i=0; i<n; i++){
+            arr[a[i]]++;
+        }
         int rep= -1;
         int mis= -1;
-        for(int i=1; i<=a.length; i++){
-            int count= 0;
-            for(int j=0; j<a.length; j++){
-                if(a[j]==i){
-                    count++;
-                }
-            }
-            if(count==2){
+        for(int i=1; i<=n; i++){
+            if(arr[i]==2){
                 rep= i;
             }
-            else if(count==0){
+            else if(arr[i]==0){
                 mis= i;
             }
-            if(count==2 && mis==0){
+            if(rep!=-1 && mis!=-1){
                 break;
             }
         }
