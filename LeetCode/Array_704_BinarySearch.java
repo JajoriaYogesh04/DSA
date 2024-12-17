586417e5419f94c98d6b0ac3748613f433e1e78e
JajoriaYogesh04 <yogeshjajoria04@gmail.com>
2024-12-17 07:08:25 +0530

    lc: 704: binary search

--- LeetCode/Array_704_BinarySearch.java
+++ LeetCode/Array_704_BinarySearch.java
@@ -1,24 +1,19 @@
-public class Array_704_BinarySearch {
-    public static int search(int[] nums, int target) {
-        int si=0;
-        int ei= nums.length-1;
-        while(si<=ei){
-            int mid= si+(ei-si)/2;
+class Solution {
+    public int search(int[] nums, int target) {
+        int start= 0;
+        int end= nums.length-1;
+        while(start<=end){
+            int mid= start+(end-start)/2;
             if(nums[mid]==target){
                 return mid;
             }
-            else if(nums[mid]<target){
-                si= mid+1;
+            else if(target<nums[mid]){
+                end= mid-1;
             }
             else{
-                ei= mid-1;
+                start= mid+1;
             }
         }
         return -1;
-    }
-    public static void main(String args[]){
-        int nums[]= {-1,0,3,5,9,12};
-        int target= 9;
-        System.out.println(search(nums, target));
     }
 }
