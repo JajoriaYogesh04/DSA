1c4948a3b95d5d35223cd4d8b2a88d90fed2f27f
JajoriaYogesh04 <yogeshjajoria04@gmail.com>
2024-12-22 21:58:59 +0530

    cn: best time to buy and sell stocks modified

--- CodingNinja/BestTimeToBuyAndSellStock.java
+++ CodingNinja/BestTimeToBuyAndSellStock.java
@@ -1,3 +1,27 @@
+// import java.util.* ;
+// import java.io.*; 
+// import java.util.ArrayList;
+
+// public class Solution{
+//     public static int maximumProfit(ArrayList<Integer> prices){
+//         // Write your code here.
+//         int buy= Integer.MAX_VALUE;
+//         int profit= 0;
+//         for(int i=0; i<prices.size(); i++){
+//             int rate= prices.get(i);
+//             if(rate<buy){
+//                 buy= rate;
+//             }
+//             if(rate-buy>profit){
+//                 profit= rate-buy;
+//             }
+//         }
+//         return profit; 
+//     }
+// }
+
+
+
 import java.util.* ;
 import java.io.*; 
 import java.util.ArrayList;
@@ -5,14 +29,14 @@ import java.util.ArrayList;
 public class Solution{
     public static int maximumProfit(ArrayList<Integer> prices){
         // Write your code here.
-        int buy= Integer.MAX_VALUE;
+        int buy= prices.get(0);
         int profit= 0;
-        for(int i=0; i<prices.size(); i++){
+        for(int i=1; i<prices.size(); i++){
             int rate= prices.get(i);
             if(rate<buy){
                 buy= rate;
             }
-            if(rate-buy>profit){
+            else if(rate-buy>profit){
                 profit= rate-buy;
             }
         }
