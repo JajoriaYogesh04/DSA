a17070e38a2858f56c9755774fc90bf63b8c4878
JajoriaYogesh04 <yogeshjajoria04@gmail.com>
2024-12-24 16:44:48 +0530

    inorder traversal

--- Chapter-27/BuildTreePreorder.java
+++ Chapter-27/BuildTreePreorder.java
@@ -30,13 +30,27 @@ public class BuildTreePreorder{
             preorder(root.left);
             preorder(root.right);
         }
+        public static void inorder(Node root){
+            if(root==null){
+                // System.out.print("-1 ");
+                return;
+            }
+            inorder(root.left);
+            System.out.print(root.data+" ");
+            inorder(root.right);
+        }
     }
     public static void main(String args[]){
         int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BinaryTree tree= new BinaryTree();
         Node root= tree.buildTree(nodes);
         // System.out.println(root.data);
+
         System.out.print("PreOrder: ");
         tree.preorder(root);
+        System.out.println();
+
+        System.out.print("Inorder: ");
+        tree.inorder(root);
     }
 }
