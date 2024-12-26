00ed5e6a525dec925e4c5378b9ee629c38409f47
JajoriaYogesh04 <yogeshjajoria04@gmail.com>
2024-12-26 07:15:42 +0530

    cn: find minimum is rotated sorted array

---
+++ CodingNinja/FindMinimumInRotatedSortedArray.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public static int findMin(int []arr) {
+        // Write your code here.
+        int si=0;
+        int ei=arr.length-1;
+        int min=Integer.MAX_VALUE;
+        while(si<=ei){
+            int mi=si+(ei-si)/2;
+            if(arr[si]<=arr[ei]){
+                min=Math.min(min,arr[si]);
+                break;
+            }
+            if(arr[si]<=arr[mi]){
+                min=Math.min(min,arr[si]);
+                si=mi+1;
+            }
+            else{
+                min=Math.min(min,arr[mi]);
+                ei=mi-1;
+            }
+        }
+        return min;
+    }
+}
