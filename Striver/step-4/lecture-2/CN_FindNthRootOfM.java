f9a0c735950e0832c0b7bcec9161b942a49b7c47
JajoriaYogesh04 <yogeshjajoria04@gmail.com>
2025-01-02 20:05:56 +0530

    striver: cn: find nth root of a number using binary search

--- Striver/step-4/lecture-2/CN_FindNthRootOfM.java
+++ Striver/step-4/lecture-2/CN_FindNthRootOfM.java
@@ -1,16 +1,53 @@
+// public class Solution {
+//     public static int NthRoot(int n, int m) {
+//         // Write your code here.
+//         for(int i=1; i<=m; i++){
+//             int mul=1;
+//             for(int j=0; j<n; j++){
+//                 mul*=i;
+//             }
+//             if(mul==m){
+//                 return i;
+//             }
+//             else if(mul>m){
+//                 break; 
+//             }
+//         }
+//         return -1;
+//     }
+// }
+
+
+
 public class Solution {
+    public static int power(int base, int pow, int num){
+        long ans=1;
+        for(int i=0; i<pow; i++){
+            ans*=base;
+            if(ans>num){
+                return 2;
+            }
+        }
+        if(ans==num){
+            return 1;
+        }
+        return 0;
+    }
     public static int NthRoot(int n, int m) {
         // Write your code here.
-        for(int i=1; i<=m; i++){
-            int mul=1;
-            for(int j=0; j<n; j++){
-                mul*=i;
+        int si=1;
+        int ei=m;
+        while(si<=ei){
+            int mi=si+(ei-si)/2;
+            int ans= power(mi, n, m);
+            if(ans==1){
+                return mi;
             }
-            if(mul==m){
-                return i;
+            else if(ans==2){
+                ei=mi-1;
             }
-            else if(mul>m){
-                break; 
+            else if(ans==0){
+                si=mi+1;
             }
         }
         return -1;
