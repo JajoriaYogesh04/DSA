d1e95b3d44c8cf1bc7f79142cd585be3f8d7cfbb
JajoriaYogesh04 <yogeshjajoria04@gmail.com>
2025-01-03 21:28:15 +0530

    striver: cn: koko eating bananas(optimal approach)

--- Striver/step-4/lecture-2/CN_KokoEatingBananas.java
+++ Striver/step-4/lecture-2/CN_KokoEatingBananas.java
@@ -1,20 +1,49 @@
+// public class Solution {
+//     public static int minimumRateToEatBananas(int []v, int h) {
+//         // Write Your Code Here
+//         int k=Integer.MIN_VALUE;
+//         for(int i=0; i<v.length; i++){
+//             k= Math.max(k, v[i]);
+//         }
+//         for(int i=1; i<=k; i++){
+//             int sum=0;
+//             for(int j=0; j<v.length; j++){
+//                 sum+=Math.ceil((double)v[j]/(double)i);
+//             }
+//             if(sum<=h){
+//                 return i;
+//             }
+//         }
+//         return -1;
+//     }
+// }
+
+
+
 public class Solution {
     public static int minimumRateToEatBananas(int []v, int h) {
         // Write Your Code Here
-        int k=Integer.MIN_VALUE;
+        int ei=Integer.MIN_VALUE;
         for(int i=0; i<v.length; i++){
-            k= Math.max(k, v[i]);
+            ei= Math.max(ei, v[i]);
         }
-        for(int i=1; i<=k; i++){
-            int sum=0;
+        int si=1;
+        int ans=-1;
+        while(si<=ei){
+            int mi=si+(ei-si)/2;
+            long time=0;
             for(int j=0; j<v.length; j++){
-                sum+=Math.ceil((double)v[j]/(double)i);
+                time+=(v[j]+mi-1)/mi;
             }
-            if(sum<=h){
-                return i;
+            if(time<=h){
+                ans=mi;
+                ei=mi-1;
+            }
+            else{
+                si=mi+1;
             }
         }
-        return -1;
+        return ans;
     }
 }
 
