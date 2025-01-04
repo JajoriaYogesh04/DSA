e0acfc881c04878115fe12fccd6487fda4d8e5ac
JajoriaYogesh04 <yogeshjajoria04@gmail.com>
2025-01-04 12:35:46 +0530

    cn: lca of binary tree(optimized)

--- CodingNinja/LCAofBinaryTree.java
+++ CodingNinja/LCAofBinaryTree.java
@@ -1,3 +1,56 @@
+// /************************************************************
+
+//  Following is the TreeNode class structure
+
+//  class TreeNode<T>
+//  {
+//      T data;
+//      TreeNode<T> left;
+//      TreeNode<T> right;
+
+//      TreeNode(T data)
+//      {
+//          this.data = data;
+//          left = null;
+//          right = null;
+//      }
+//  };
+
+//  ************************************************************/
+// import java.util.*;
+// public class Solution {
+//     public static boolean getPath(TreeNode<Integer> root, int node, ArrayList<Integer> list){
+//         if(root==null){
+//             return false;
+//         }    
+//         list.add(root.data);
+//         if(root.data==node){
+//             return true;
+//         }
+//         if(getPath(root.left, node, list)||getPath(root.right, node, list)){
+//             return true;
+//         }
+//         list.remove(list.size()-1);
+//         return false;
+//     }
+//     public static int lowestCommonAncestor(TreeNode<Integer> root, int x, int y) {
+//         //    Write your code here.
+//         ArrayList<Integer> path1= new ArrayList<>();
+//         ArrayList<Integer> path2= new ArrayList<>();
+//         getPath(root, x, path1);
+//         getPath(root, y, path2);
+//         int i=0;
+//         for(;(i<path1.size() && i<path2.size()); i++){
+//             if(path1.get(i)!=path2.get(i)){
+//                 break;
+//             }
+//         }
+//         return path1.get(i-1);
+//     }
+// }
+
+
+
 /************************************************************
 
  Following is the TreeNode class structure
@@ -17,34 +70,24 @@
  };
 
  ************************************************************/
-import java.util.*;
+
 public class Solution {
-    public static boolean getPath(TreeNode<Integer> root, int node, ArrayList<Integer> list){
-        if(root==null){
-            return false;
-        }    
-        list.add(root.data);
-        if(root.data==node){
-            return true;
+    public static TreeNode<Integer> helper(TreeNode<Integer> root, int x, int y){
+        if(root==null||root.data==x||root.data==y){
+            return root;
         }
-        if(getPath(root.left, node, list)||getPath(root.right, node, list)){
-            return true;
+        TreeNode<Integer> leftlca= helper(root.left, x, y);
+        TreeNode<Integer> rightlca= helper(root.right, x, y);
+        if(leftlca==null){
+            return rightlca;
         }
-        list.remove(list.size()-1);
-        return false;
+        if(rightlca==null){
+            return leftlca;
+        }
+        return root;
     }
     public static int lowestCommonAncestor(TreeNode<Integer> root, int x, int y) {
         //    Write your code here.
-        ArrayList<Integer> path1= new ArrayList<>();
-        ArrayList<Integer> path2= new ArrayList<>();
-        getPath(root, x, path1);
-        getPath(root, y, path2);
-        int i=0;
-        for(;(i<path1.size() && i<path2.size()); i++){
-            if(path1.get(i)!=path2.get(i)){
-                break;
-            }
-        }
-        return path1.get(i-1);
+        return helper(root, x, y).data;
     }
 }
