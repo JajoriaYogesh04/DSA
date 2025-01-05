d05a2dc49d400ba559996eb2a95ee2d03de7cfb9
JajoriaYogesh04 <yogeshjajoria04@gmail.com>
2025-01-05 19:15:40 +0530

    striver: cn: minimum days to make m bouquets(optimized)

--- Striver/step-4/lecture-2/CN_RoseGarden.java
+++ Striver/step-4/lecture-2/CN_RoseGarden.java
@@ -1,3 +1,40 @@
+// public class Solution {
+//     public static boolean isPossible(int arr[],int i,int r,int b){
+//         int count=0;
+//         int boq=0;
+//         for(int j=0;j<arr.length;j++){
+//             if(arr[j]<=i){
+//                 count++;
+//             }
+//             else{
+//                 boq+=(count/r);
+//                 count=0;
+//             }
+//         }
+//         boq+=(count/r);
+//         if(boq>=b){
+//             return true;
+//         }
+//         return false;
+//     }
+//     public static int roseGarden(int[] arr, int r, int b) {
+//         // Write your code here.
+//         int min=Integer.MAX_VALUE;
+//         int max=Integer.MIN_VALUE;
+//         for(int i=0;i<arr.length;i++){
+//             min=Math.min(arr[i],min);
+//             max=Math.max(arr[i],max);
+//         }
+//         for(int i=min;i<=max;i++){
+//             if(isPossible(arr, i, r, b)){
+//                 return i;
+//             }
+//         }
+//         return -1;
+//     }
+// }
+
+
 public class Solution {
     public static boolean isPossible(int arr[],int i,int r,int b){
         int count=0;
@@ -19,17 +56,24 @@ public class Solution {
     }
     public static int roseGarden(int[] arr, int r, int b) {
         // Write your code here.
-        int min=Integer.MAX_VALUE;
-        int max=Integer.MIN_VALUE;
-        for(int i=0;i<arr.length;i++){
-            min=Math.min(arr[i],min);
-            max=Math.max(arr[i],max);
+        if(r*b>arr.length){
+            return -1;
         }
-        for(int i=min;i<=max;i++){
-            if(isPossible(arr, i, r, b)){
-                return i;
+        int si=Integer.MAX_VALUE;
+        int ei=Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            si=Math.min(arr[i],si);
+            ei=Math.max(arr[i],ei);
+        }
+        while(si<=ei){
+            int mi=si+(ei-si)/2;
+            if(isPossible(arr, mi, r, b)){
+                ei=mi-1;
+            }
+            else{
+                si=mi+1;
             }
         }
-        return -1;
+        return si;
     }
 }
