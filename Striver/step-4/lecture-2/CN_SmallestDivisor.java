6d0425c729fc8249f82b5e22dfb0af5e5eedd3f0
JajoriaYogesh04 <yogeshjajoria04@gmail.com>
2025-01-06 19:31:32 +0530

    striver: cn: find the smallest divisor

---
+++ Striver/step-4/lecture-2/CN_SmallestDivisor.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public static boolean isPossible(int arr[],int div,int limit){
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum+=Math.ceil((double)arr[i]/div);
+        }
+        return sum<=limit;
+    }
+    public static int smallestDivisor(int arr[], int limit) {
+        // Write your coder here
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            max=Math.max(arr[i],max);
+        }
+        for(int i=1;i<=max;i++){
+            if(isPossible(arr, i, limit)){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
