d6e54155389d46e3e1543d5023edaa93864be4ef
JajoriaYogesh04 <yogeshjajoria04@gmail.com>
2025-01-06 19:32:12 +0530

    striver: cn: find the smallest divisor(optimal)

--- Striver/step-4/lecture-2/CN_SmallestDivisor.java
+++ Striver/step-4/lecture-2/CN_SmallestDivisor.java
@@ -1,3 +1,28 @@
+// public class Solution {
+//     public static boolean isPossible(int arr[],int div,int limit){
+//         int sum=0;
+//         for(int i=0;i<arr.length;i++){
+//             sum+=Math.ceil((double)arr[i]/div);
+//         }
+//         return sum<=limit;
+//     }
+//     public static int smallestDivisor(int arr[], int limit) {
+//         // Write your coder here
+//         int max=Integer.MIN_VALUE;
+//         for(int i=0;i<arr.length;i++){
+//             max=Math.max(arr[i],max);
+//         }
+//         for(int i=1;i<=max;i++){
+//             if(isPossible(arr, i, limit)){
+//                 return i;
+//             }
+//         }
+//         return -1;
+//     }
+// }
+
+
+
 public class Solution {
     public static boolean isPossible(int arr[],int div,int limit){
         int sum=0;
@@ -8,15 +33,20 @@ public class Solution {
     }
     public static int smallestDivisor(int arr[], int limit) {
         // Write your coder here
-        int max=Integer.MIN_VALUE;
+        int ei=Integer.MIN_VALUE;
         for(int i=0;i<arr.length;i++){
-            max=Math.max(arr[i],max);
+            ei=Math.max(arr[i],ei);
         }
-        for(int i=1;i<=max;i++){
-            if(isPossible(arr, i, limit)){
-                return i;
+        int si=1;
+        while(si<=ei){
+            int mi=si+(ei-si)/2;
+            if(isPossible(arr, mi, limit)){
+                ei=mi-1;
+            }
+            else{
+                si=mi+1;
             }
         }
-        return -1;
+        return si;
     }
 }
