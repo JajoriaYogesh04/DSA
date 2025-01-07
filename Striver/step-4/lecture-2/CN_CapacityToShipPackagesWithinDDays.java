0209862a136d189a5ac181e5828c888625f5240b
JajoriaYogesh04 <yogeshjajoria04@gmail.com>
2025-01-07 21:36:11 +0530

    striver: cn: capacity to ship packages within d days(optimal)

--- Striver/step-4/lecture-2/CN_CapacityToShipPackagesWithinDDays.java
+++ Striver/step-4/lecture-2/CN_CapacityToShipPackagesWithinDDays.java
@@ -1,7 +1,46 @@
+// import java.util.* ;
+// import java.io.*; 
+// public class Solution {
+//     public static int isPossible(int arr[],int limit,int d){
+//         int sum=0;
+//         int count=1;
+//         for(int i=0;i<arr.length;i++){
+//             if(sum+arr[i]<=limit){
+//                 sum+=arr[i];
+//             }
+//             else{
+//                 count++;
+//                 sum=arr[i];
+//                 if(count>d){
+//                     break;
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+//     public static int leastWeightCapacity(int[] weights, int d) {
+//         // Write your code here.
+//         int si=Integer.MIN_VALUE;
+//         int ei=0;
+//         for(int i=0;i<weights.length;i++){
+//             si=Math.max(si,weights[i]);
+//             ei+=weights[i];
+//         }
+//         for(int i=si;i<=ei;i++){
+//             if(isPossible(weights, i, d)<=d){
+//                 return i;
+//             }
+//         }
+//         return -1;
+//     }
+// }
+
+
+
 import java.util.* ;
 import java.io.*; 
 public class Solution {
-    public static int isPossible(int arr[],int limit,int d){
+    public static int countDays(int arr[],int limit,int d){
         int sum=0;
         int count=1;
         for(int i=0;i<arr.length;i++){
@@ -26,11 +65,15 @@ public class Solution {
             si=Math.max(si,weights[i]);
             ei+=weights[i];
         }
-        for(int i=si;i<=ei;i++){
-            if(isPossible(weights, i, d)<=d){
-                return i;
+        while(si<=ei){
+            int mi=si+(ei-si)/2;
+            if(countDays(weights,mi,d)<=d){
+                ei=mi-1;
+            }
+            else{
+                si=mi+1;
             }
         }
-        return -1;
+        return si;
     }
 }
