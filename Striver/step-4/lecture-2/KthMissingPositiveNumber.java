fdb72cb2e010a70ae4840fd3c4427d10d0f9588d
JajoriaYogesh04 <yogeshjajoria04@gmail.com>
2025-01-08 19:49:10 +0530

    striver: cn: kth missing positive number(optimal)

--- Striver/step-4/lecture-2/KthMissingPositiveNumber.java
+++ Striver/step-4/lecture-2/KthMissingPositiveNumber.java
@@ -1,14 +1,38 @@
+// public class Solution {
+//     public static int missingK(int[] vec, int n, int k) {
+//         // Write your code here.
+//         for(int i=0;i<vec.length;i++){
+//             if(vec[i]<=k){
+//                 k++;
+//             }
+//             else{
+//                 break;
+//             }
+//         }
+//         return k;
+//     }
+// }
+
+
+
 public class Solution {
     public static int missingK(int[] vec, int n, int k) {
         // Write your code here.
-        for(int i=0;i<vec.length;i++){
-            if(vec[i]<=k){
-                k++;
+        if(k<vec[0]){
+            return k;
+        }
+        int si=0;
+        int ei=n-1;
+        while(si<=ei){
+            int mi=si+(ei-si)/2;
+            int miss= vec[mi]-(mi+1);
+            if(miss<k){
+                si=mi+1;
             }
             else{
-                break;
+                ei=mi-1;
             }
         }
-        return k;
+        return ei+k+1;
     }
 }
