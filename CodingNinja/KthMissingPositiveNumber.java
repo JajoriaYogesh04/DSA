aa665500390c4407d9038acc1469324bff6f27dd
JajoriaYogesh04 <yogeshjajoria04@gmail.com>
2025-01-08 19:50:16 +0530

    cn: kth missing positive number

---
+++ CodingNinja/KthMissingPositiveNumber.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public static int missingK(int[] vec, int n, int k) {
+        // Write your code here.
+        if(k<vec[0]){
+            return k;
+        }
+        int si=0;
+        int ei=n-1;
+        while(si<=ei){
+            int mi=si+(ei-si)/2;
+            int miss= vec[mi]-(mi+1);
+            if(miss<k){
+                si=mi+1;
+            }
+            else{
+                ei=mi-1;
+            }
+        }
+        return ei+k+1;
+    }
+}
