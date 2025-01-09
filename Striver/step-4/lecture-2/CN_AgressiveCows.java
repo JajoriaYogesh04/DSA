20cb3acc0f286076194eda5292a20f25e2bc5c03
JajoriaYogesh04 <yogeshjajoria04@gmail.com>
2025-01-09 19:42:49 +0530

    striver: cn: aggressive cows(optimal)

--- Striver/step-4/lecture-2/CN_AgressiveCows.java
+++ Striver/step-4/lecture-2/CN_AgressiveCows.java
@@ -1,3 +1,39 @@
+// import java.util.*;
+// public class Solution {
+//     public static boolean canPlace(int arr[],int dist,int k){
+//         int count=1;
+//         int last=arr[0];
+//         for(int i=1;i<arr.length;i++){
+//             if(arr[i]-last>=dist){
+//                 count++;
+//                 last=arr[i];
+//             }
+//             if(count>=k){
+//                 break;
+//             }
+//         }
+//         return count>=k;
+//     }
+//     public static int aggressiveCows(int []stalls, int k) {
+//         //    Write your code here.
+//         Arrays.sort(stalls);
+//         int n=stalls.length;
+//         int ans=-1;
+//         for(int i=1; i<=(stalls[n-1]-stalls[0]);i++){
+//             if(canPlace(stalls,i,k)){
+//                 ans=i; 
+//             }
+//             else{
+//                 break;
+//             }
+//         }
+//         return ans;
+//     }
+// }
+
+
+
+
 import java.util.*;
 public class Solution {
     public static boolean canPlace(int arr[],int dist,int k){
@@ -9,24 +45,26 @@ public class Solution {
                 last=arr[i];
             }
             if(count>=k){
-                break;
+                return true;
             }
         }
-        return count>=k;
+        return false;
     }
     public static int aggressiveCows(int []stalls, int k) {
         //    Write your code here.
         Arrays.sort(stalls);
         int n=stalls.length;
-        int ans=-1;
-        for(int i=1; i<=(stalls[n-1]-stalls[0]);i++){
-            if(canPlace(stalls,i,k)){
-                ans=i; 
+        int si=1;
+        int ei=stalls[n-1]-stalls[0];
+        while(si<=ei){
+            int mi=si+(ei-si)/2;
+            if(canPlace(stalls,mi,k)){
+                si=mi+1;
             }
             else{
-                break;
+                ei=mi-1;
             }
         }
-        return ans;
+        return ei;
     }
 }
