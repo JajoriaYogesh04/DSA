7979eec1f4d01a785a4fa0d512c2b186de71f4b2
JajoriaYogesh04 <yogeshjajoria04@gmail.com>
2025-01-10 19:42:13 +0530

    cn: allocate books

---
+++ CodingNinja/AllocateBooks.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+public class Solution {
+    public static int countStudents(ArrayList<Integer>arr,int m,int pages){
+        int count=1;
+        long sum=0;
+        for(int i=0;i<arr.size();i++){
+            if(arr.get(i)+sum<=pages){
+                sum+=arr.get(i);
+            }
+            else{
+                sum=arr.get(i);
+                count++;
+                if(count>m){
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+    public static int findPages(ArrayList<Integer> arr, int n, int m) {
+        // Write your code here.
+        if(m>n) return -1;
+        int si=Integer.MIN_VALUE;
+        int ei=0;
+        for(int i=0;i<arr.size();i++){
+            si=Math.max(arr.get(i),si);
+            ei+=arr.get(i);
+        }
+        while(si<=ei){
+            int mi=si+(ei-si)/2;
+            if(countStudents(arr,m,mi)<=m){
+                ei=mi-1;
+            }
+            else{
+                si=mi+1;
+            }
+        }
+        return si;
+    }
+}
